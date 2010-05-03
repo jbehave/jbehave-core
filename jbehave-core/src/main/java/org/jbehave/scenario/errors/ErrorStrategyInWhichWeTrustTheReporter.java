0833c76c780456bc8c60f862536f91e5be914aa0
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-05-03 11:50:52 +0200

    Removed org.jbehave.scenario classes and its usages that had not been removed in the merge with jbehave-3.0-beta-3 branch

--- jbehave-core/src/main/java/org/jbehave/scenario/errors/ErrorStrategyInWhichWeTrustTheReporter.java
+++
@@ -1,7 +0,0 @@
-package org.jbehave.scenario.errors;
-
-public class ErrorStrategyInWhichWeTrustTheReporter implements ErrorStrategy {
-    public void handleError(Throwable throwable) throws Throwable {
-        throw new AssertionError("An error occurred while running the scenarios; please check output for details.");
-    }
-}
