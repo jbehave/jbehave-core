adedbcdb3a496a2bfd1233fa8d3539b58c43b574
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-09-25 16:35:49 +0200

    Updated upload script to update the qualifier symlink

--- distribution/upload-reference.sh
+++ distribution/upload-reference.sh
@@ -1,11 +1,16 @@
 
 VERSION=$1
+QUALIFIER=$2
 
-if [ "$VERSION" == "" ]; then
-  echo "usage: upload-reference.sh <version>"
+if [ "$VERSION" == "" ] || [ "$QUALIFIER" == "" ]; then
+  echo "usage: upload-reference.sh <version> <qualifier>"
   exit;
 fi
 
-scp target/jbehave-$VERSION-bin.zip jbehave.org:
+ARTIFACT="jbehave-$VERSION"
+REFERENCE="/var/www/jbehave.org/reference"
 
-ssh jbehave.org "rm -r jbehave-$VERSION; unzip jbehave-$VERSION-bin.zip; rm -r /var/www/jbehave.org/reference/$VERSION;  mv jbehave-$VERSION/docs/ /var/www/jbehave.org/reference/$VERSION"
+VERSIONED="$REFERENCE/$VERSION"
+
+scp target/$ARTIFACT-bin.zip jbehave.org:
+ssh jbehave.org "rm -r $ARTIFACT; unzip $ARTIFACT-bin.zip; rm -r $VERSIONED; mv $ARTIFACT/docs/ $VERSIONED; cd $REFERENCE; rm $QUALIFIER; ln -s $VERSION $QUALIFIER"
