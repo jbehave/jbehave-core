3c23fd51ca199107bad4f65b1caffe199d5b498a
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-09-25 10:40:49 +0200

    JBEHAVE-349:  Better handling of corner cases in spring context, e.g. undefined types and runtime exceptions in getting bean.

--- jbehave-spring/src/main/java/org/jbehave/core/steps/spring/SpringStepsFactory.java
+++ jbehave-spring/src/main/java/org/jbehave/core/steps/spring/SpringStepsFactory.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.steps.AbstractStepsFactory;
 import org.jbehave.core.steps.InjectableStepsFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -31,15 +32,27 @@ public class SpringStepsFactory extends AbstractStepsFactory {
         List<Object> steps = new ArrayList<Object>();
         for (String name : context.getBeanDefinitionNames()) {
             Class<?> type = context.getType(name);            
-            if ( isNotAbstract(type) && hasAnnotatedMethods(type)) {
-                steps.add(context.getBean(name));
+            if ( isAllowed(type) && hasAnnotatedMethods(type)) {
+                try {
+                    steps.add(context.getBean(name));
+                } catch ( BeansException e ){
+                    // failed to get bean instance for whatever reason
+                    // we ignore it and move on
+                }
             }
         }
         return steps;
     }
 
-    protected boolean isNotAbstract(Class<?> type) {
-        return !Modifier.isAbstract(type.getModifiers());
+    /**
+     * Checks if type returned from context is allowed,
+     * i.e. not null and not abstract.
+     * 
+     * @param type the Class of the bean
+     * @return A boolean, <code>true</code> if allowed
+     */
+    protected boolean isAllowed(Class<?> type) {
+        return type != null && !Modifier.isAbstract(type.getModifiers());
     }
 
 }
--- jbehave-spring/src/test/java/org/jbehave/core/steps/spring/SpringStepsFactoryBehaviour.java
+++ jbehave-spring/src/test/java/org/jbehave/core/steps/spring/SpringStepsFactoryBehaviour.java
@@ -1,72 +1,89 @@
 package org.jbehave.core.steps.spring;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.Steps;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.context.ApplicationContext;
 
 public class SpringStepsFactoryBehaviour {
 
-    private static Field stepsInstance;
-
-    @Before
-    public void setUp() throws NoSuchFieldException {
-        stepsInstance = Steps.class.getDeclaredField("instance");
-        stepsInstance.setAccessible(true);
-    }
-
     private ApplicationContext createApplicationContext(String... resourceLocations) {
         return new SpringApplicationContextFactory(resourceLocations).createApplicationContext();
     }
 
     @Test
-    public void assertThatStepsCanBeCreated() throws NoSuchFieldException, IllegalAccessException {
+    public void stepsCanBeCreated() {
         // Given
         ApplicationContext context = createApplicationContext("org/jbehave/core/steps/spring/steps.xml");
         SpringStepsFactory factory = new SpringStepsFactory(new MostUsefulConfiguration(), context);
         // When
         List<CandidateSteps> steps = factory.createCandidateSteps();
-        // Then 
+        // Then
         assertFooStepsFound(steps);
     }
 
-
     @Test
-    public void assertThatStepsWithStepsWithDependencyCanBeCreated() throws NoSuchFieldException, IllegalAccessException {
+    public void stepsWithDependenciesCanBeCreated() {
+        // Given
         ApplicationContext context = createApplicationContext("org/jbehave/core/steps/spring/steps-with-dependency.xml");
         // When
         SpringStepsFactory factory = new SpringStepsFactory(new MostUsefulConfiguration(), context);
         List<CandidateSteps> steps = factory.createCandidateSteps();
         // Then
         assertFooStepsFound(steps);
-        assertEquals(42, (int) ((FooStepsWithDependency) stepsInstance.get(steps.get(0))).integer);
+        assertEquals(42, (int) ((FooStepsWithDependency) firstStepsInstance(steps)).integer);
     }
 
-    private void assertFooStepsFound(List<CandidateSteps> steps) throws NoSuchFieldException, IllegalAccessException {
+    private void assertFooStepsFound(List<CandidateSteps> steps) {
         assertEquals(1, steps.size());
-        assertTrue(steps.get(0) instanceof CandidateSteps);
-        Object instance = stepsInstance.get(steps.get(0));
-        assertTrue(instance instanceof FooSteps);
+        assertThat(firstStepsInstance(steps), instanceOf(FooSteps.class));
     }
 
+    private Object firstStepsInstance(List<CandidateSteps> steps) {
+        return ((Steps) steps.get(0)).instance();
+    }
 
-    @Test(expected=BeanDefinitionStoreException.class)
-    public void assertThatStepsWithMissingDependenciesCannotBeCreated() throws NoSuchFieldException, IllegalAccessException {
+    @Test(expected = BeanDefinitionStoreException.class)
+    public void stepsWithMissingDependenciesCannotBeCreated() {
+        // Given
         ApplicationContext context = createApplicationContext("org/jbehave/core/steps/spring/steps-with-missing-depedency.xml");
         SpringStepsFactory factory = new SpringStepsFactory(new MostUsefulConfiguration(), context);
         // When
         factory.createCandidateSteps();
-        // Then ... expected exception is thrown        
+        // Then ... expected exception is thrown
+    }
+
+    @Test
+    public void beansWithUndefinedTypeOrCannotBeCreatedWillBeIgnored() {
+        // Given
+        ApplicationContext context = mock(ApplicationContext.class);
+        SpringStepsFactory factory = new SpringStepsFactory(new MostUsefulConfiguration(), context);
+        // When
+        when(context.getBeanDefinitionNames()).thenReturn(new String[] { "fooSteps", "undefined", "blowUp" });
+        when(context.getType("fooSteps")).thenReturn(FooSteps.class);
+        when(context.getType("undefined")).thenReturn(null);
+        when(context.getType("blowUp")).thenReturn(BlowUp.class);
+        when(context.getBean("fooSteps")).thenReturn(new FooSteps());
+        when(context.getBean("blowUp")).thenThrow(new RuntimeException("Bum!"));
+        List<CandidateSteps> candidateSteps = factory.createCandidateSteps();
+        // Then
+        assertThat(candidateSteps.size(), equalTo(1));
+        assertThat(firstStepsInstance(candidateSteps), instanceOf(FooSteps.class));
+        verify(context, never()).getBean("undefined");
     }
 
     public static class FooSteps {
@@ -85,8 +102,12 @@ public class SpringStepsFactoryBehaviour {
         }
 
     }
-    
+
     public static abstract class AbstractSteps {
-        
+
+    }
+
+    public static class BlowUp {
+
     }
 }
