331618ace99c3890c1902a65946bbdfac6cf770d
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-12-12 01:14:38 +0100

    JBEHAVE-403:  Added NullStoryReporter.

---
+++ jbehave-core/src/main/java/org/jbehave/core/reporters/NullStoryReporter.java
@@ -0,0 +1,79 @@
+package org.jbehave.core.reporters;
+
+import java.util.List;
+import java.util.Map;
+
+import org.jbehave.core.model.ExamplesTable;
+import org.jbehave.core.model.GivenStories;
+import org.jbehave.core.model.Meta;
+import org.jbehave.core.model.OutcomesTable;
+import org.jbehave.core.model.Scenario;
+import org.jbehave.core.model.Story;
+
+/**
+ * <p>
+ * <a href="http://en.wikipedia.org/wiki/Null_Object_pattern">Null-object</a> implementation of 
+ * {@link StoryReporter}.   Users can subclass it and can override only the method that they
+ * are interested in.
+ * </p>
+ */
+public class NullStoryReporter implements StoryReporter {
+
+    public void successful(String step) {
+    }
+
+    public void ignorable(String step) {
+    }
+
+    public void pending(String step) {
+    }
+
+    public void notPerformed(String step) {
+    }
+
+    public void failed(String step, Throwable cause) {
+    }
+
+    public void failedOutcomes(String step, OutcomesTable table) {
+    }
+
+    public void beforeStory(Story story, boolean givenStory) {
+    }
+
+    public void storyNotAllowed(Story story, String filter) {
+    }
+
+    public void afterStory(boolean givenStory) {
+    }
+
+    public void givenStories(GivenStories givenStories) {
+    }
+
+    public void givenStories(List<String> storyPaths) {
+    }
+
+    public void beforeScenario(String title, boolean givenStory) {
+    }
+
+    public void scenarioNotAllowed(Scenario scenario, String filter, boolean givenStory) {
+    }
+
+    public void scenarioMeta(Meta meta, boolean givenStory) {
+    }
+
+    public void afterScenario(boolean givenStory) {
+    }
+
+    public void beforeExamples(List<String> steps, ExamplesTable table) {
+    }
+
+    public void example(Map<String, String> tableRow) {
+    }
+
+    public void afterExamples() {
+    }
+
+    public void dryRun() {
+    }
+
+}
---
+++ jbehave-core/src/test/java/org/jbehave/core/reporters/NullStoryReporterBehaviour.java
@@ -0,0 +1,78 @@
+package org.jbehave.core.reporters;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import org.hamcrest.Matchers;
+import org.jbehave.core.model.Description;
+import org.jbehave.core.model.ExamplesTable;
+import org.jbehave.core.model.Narrative;
+import org.jbehave.core.model.OutcomesTable;
+import org.jbehave.core.model.OutcomesTable.OutcomesFailed;
+import org.jbehave.core.model.Scenario;
+import org.jbehave.core.model.Story;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class NullStoryReporterBehaviour {
+
+    @Test
+    public void shouldOnlyReportOverriddenMethods() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream(out);
+        StoryReporter reporter = new NullStoryReporter(){
+
+            @Override
+            public void beforeStory(Story story, boolean givenStory) {
+                printStream.println("beforeStory");
+            }
+
+            @Override
+            public void afterStory(boolean givenStory) {
+                printStream.println("afterStory");
+            }
+            
+        };
+        narrateAnInterestingStory(reporter);
+        assertThat(out.toString(), Matchers.equalTo("beforeStory\nafterStory\n"));
+    }
+
+
+    private void narrateAnInterestingStory(StoryReporter reporter) {
+        Story story = new Story("/path/to/story", new Description("An interesting story"), new Narrative(
+                "renovate my house", "customer", "get a loan"), new ArrayList<Scenario>());
+        reporter.dryRun();
+        reporter.beforeStory(story, false);
+
+        reporter.successful("Given I have a balance of $50");
+        reporter.ignorable("!-- A comment");
+        reporter.successful("When I request $20");
+        reporter.successful("When I ask Liz for a loan of $100");
+        reporter.afterScenario(false);
+
+        // 2nd scenario
+        reporter.beforeScenario("A failing scenario", false);
+        OutcomesTable outcomesTable = new OutcomesTable();
+        outcomesTable.addOutcome("I don't return all", 100.0, equalTo(50.));
+        try {
+            outcomesTable.verify();
+        } catch (OutcomesFailed e) {
+            reporter.failedOutcomes("Then I don't return loan", e.outcomesTable());
+        }
+        reporter.pending("Then I should have a balance of $30");
+        reporter.notPerformed("Then I should have $20");
+        ExamplesTable table = new ExamplesTable("|money|to|\n|$30|Mauro|\n|$50|Paul|\n");
+        reporter.beforeExamples(asList("Given money <money>", "Then I give it to <to>"), table);
+        reporter.example(table.getRow(0));
+        reporter.example(table.getRow(1));
+        reporter.afterExamples();
+        reporter.afterScenario(false);
+        reporter.afterStory(false);
+    }
+
+}
