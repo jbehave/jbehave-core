9b0ab057379025473adb1ffefac5f3921990059b
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-01-03 09:24:47 +0100

    Updated example using Spring JUnit runner

--- examples/trader-spring/src/main/java/org/jbehave/examples/trader/spring/TraderEmbedderWithSpringJUnit4ClassRunner.java
+++ examples/trader-spring/src/main/java/org/jbehave/examples/trader/spring/TraderEmbedderWithSpringJUnit4ClassRunner.java
@@ -1,60 +1,58 @@
 package org.jbehave.examples.trader.spring;
 
-import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
-
 import java.util.List;
 
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.StoryFinder;
-import org.jbehave.core.steps.InstanceStepsFactory;
+import org.jbehave.core.steps.spring.SpringStepsFactory;
 import org.jbehave.examples.trader.TraderEmbedder;
-import org.jbehave.examples.trader.steps.BeforeAfterSteps;
-import org.jbehave.examples.trader.steps.TraderSteps;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+
 /**
- * Run stories via Embedder using Spring's {@link SpringJUnit4ClassRunner} to inject the
- * steps instances from a given Spring context.
+ * Run stories using Spring's {@link SpringJUnit4ClassRunner} to inject the
+ * steps ApplicationContext then used to configure the candidate steps used by
+ * the TraderEmbedder.  This examples shows how the Embedder can be used within
+ * any testing framework.  Similary, we could extend a JUnit 3 test class.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/org/jbehave/examples/trader/spring/steps.xml" })
 public class TraderEmbedderWithSpringJUnit4ClassRunner {
 
     @Autowired
-    private TraderSteps traderSteps;
+    protected ApplicationContext context;
 
-    @Autowired
-    private BeforeAfterSteps beforeAndAfterSteps;
+    private Embedder embedder;
+
+    @Before
+    public void setup() {
+        embedder = new TraderEmbedder();
+        embedder.useCandidateSteps(new SpringStepsFactory(embedder.configuration(), context).createCandidateSteps());
+    }
 
     @Test
     public void runStoriesAsPaths() {
-        embedder().runStoriesAsPaths(storyPaths());
+        List<String> storyPaths = new StoryFinder().findPaths(codeLocationFromPath("../trader/src/main/java"),
+                "**/*.story", "");
+        embedder.runStoriesAsPaths(storyPaths);
     }
 
     @Test
     public void findMatchingCandidateSteps() {
-        embedder().reportMatchingStepdocs("When traders are subset to \".*y\" by name");
-        embedder().reportMatchingStepdocs("Given a step that is not matched");
+        embedder.reportMatchingStepdocs("When traders are subset to \".*y\" by name");
+        embedder.reportMatchingStepdocs("Given a step that is not matched");
     }
 
     @Test
     public void findMatchingCandidateStepsWithNoStepsInstancesProvided() {
-        new Embedder().reportMatchingStepdocs("Given a step that cannot be matched");
-    }
-
-    private Embedder embedder() {
-        Embedder embedder = new TraderEmbedder();
-        embedder.useCandidateSteps(new InstanceStepsFactory(embedder.configuration(), traderSteps, beforeAndAfterSteps)
-                .createCandidateSteps());
-        return embedder;
-    }
-
-    protected List<String> storyPaths() {
-        return new StoryFinder().findPaths(codeLocationFromPath("../trader/src/main/java"), "**/*.story", "");
+        embedder.reportMatchingStepdocs("Given a step that cannot be matched");
     }
 
 }
