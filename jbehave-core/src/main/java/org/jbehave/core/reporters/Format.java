0202b33c96f015fb7d9e9fbd2842c4dd571a22b5
Paul Hammant <paul@hammant.org>
2011-01-21 10:56:51 -0600

    JBEHAVE-418 allow formats to have stack traces

--- jbehave-core/src/main/java/org/jbehave/core/reporters/Format.java
+++ jbehave-core/src/main/java/org/jbehave/core/reporters/Format.java
@@ -1,12 +1,12 @@
 package org.jbehave.core.reporters;
 
+import java.util.Properties;
+
 public abstract class Format {
-    public static final Format CONSOLE = new Format("CONSOLE") {
-        @Override
-        public StoryReporter createStoryReporter(FilePrintStreamFactory factory, StoryReporterBuilder storyReporterBuilder) {
-            return new ConsoleOutput(storyReporterBuilder.keywords()).doReportFailureTrace(storyReporterBuilder.reportFailureTrace());
-        }
-    };
+
+    public static final Format CONSOLE = new ConsoleFormat(false);
+
+    public static final Format CONSOLE_WITH_STACK_TRACES = new ConsoleFormat(true);
 
     public static final Format IDE_CONSOLE = new Format("IDE_CONSOLE") {
         @Override
@@ -23,13 +23,9 @@ public abstract class Format {
         }
     };
 
-    public static final Format HTML = new Format("HTML") {
-        @Override
-        public StoryReporter createStoryReporter(FilePrintStreamFactory factory, StoryReporterBuilder storyReporterBuilder) {
-            factory.useConfiguration(storyReporterBuilder.fileConfiguration("html"));
-            return new HtmlOutput(factory.createPrintStream(), storyReporterBuilder.keywords()).doReportFailureTrace(storyReporterBuilder.reportFailureTrace());
-        }
-    };
+    public static final Format HTML = new HtmlFormat(false);
+
+    public static final Format HTML_WITH_STACK_TRACES = new HtmlFormat(true);
 
     public static final Format XML = new Format("XML") {
         @Override
@@ -57,5 +53,34 @@ public abstract class Format {
 
     public String name() {
         return name;
+    }
+
+    private static class ConsoleFormat extends Format {
+        private final boolean withTraces;
+
+        public ConsoleFormat(boolean withTraces) {
+            super("CONSOLE");
+            this.withTraces = withTraces;
+        }
+
+        @Override
+        public StoryReporter createStoryReporter(FilePrintStreamFactory factory, StoryReporterBuilder storyReporterBuilder) {
+            return new ConsoleOutput(new Properties(), storyReporterBuilder.keywords(), false).doReportFailureTrace(storyReporterBuilder.reportFailureTrace());
+        }
+    }
+
+    private static class HtmlFormat extends Format {
+        private boolean withTraces;
+
+        public HtmlFormat(boolean withTraces) {
+            super("HTML");
+            this.withTraces = withTraces;
+        }
+
+        @Override
+        public StoryReporter createStoryReporter(FilePrintStreamFactory factory, StoryReporterBuilder storyReporterBuilder) {
+            factory.useConfiguration(storyReporterBuilder.fileConfiguration("html"));
+            return new HtmlOutput(factory.createPrintStream(), new Properties(), storyReporterBuilder.keywords(), withTraces).doReportFailureTrace(storyReporterBuilder.reportFailureTrace());
+        }
     }
 }
