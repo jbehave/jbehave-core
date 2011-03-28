5699da322a432f9215c75783703ae38d5c6f94d5
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-03-28 23:58:38 +0200

    JBEHAVE-456:  Made LoadFromClasspath the default ResourceLoader.

--- jbehave-core/src/main/java/org/jbehave/core/model/ExamplesTableFactory.java
+++ jbehave-core/src/main/java/org/jbehave/core/model/ExamplesTableFactory.java
@@ -3,7 +3,7 @@ package org.jbehave.core.model;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.Keywords;
 import org.jbehave.core.i18n.LocalizedKeywords;
-import org.jbehave.core.io.LoadFromURL;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.ResourceLoader;
 import org.jbehave.core.steps.ParameterConverters;
 
@@ -16,7 +16,7 @@ import static org.apache.commons.lang.StringUtils.isBlank;
  * <li>table text input, i.e. any input that contains a
  * {@link Keywords#examplesTableHeaderSeparator()}</li>
  * <li>resource path input, the table as text is loaded via the
- * {@link ResourceLoader}.</li>
+ * {@link ResourceLoader} (defaulting to {@link LoadFromClasspath}).</li>
  * </ul>
  * Factory also supports optional specification of {@link ParameterConverters}
  * to allow the ExamplesTable to convert row values. 
@@ -38,11 +38,11 @@ public class ExamplesTableFactory {
     }
 
     public ExamplesTableFactory(Keywords keywords) {
-        this(keywords, new LoadFromURL(), new ParameterConverters());
+        this(keywords, new LoadFromClasspath(), new ParameterConverters());
     }
 
     public ExamplesTableFactory(ParameterConverters parameterConverters) {
-        this(new LocalizedKeywords(), new LoadFromURL(), parameterConverters);
+        this(new LocalizedKeywords(), new LoadFromClasspath(), parameterConverters);
     }
 
     public ExamplesTableFactory(ResourceLoader resourceLoader) {
