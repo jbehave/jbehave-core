f8f04bd0f698d1b3988a5fbee92c2cfe837c0754
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-03-31 11:32:40 +0200

    Refactored hudson trasformation tests to not use static methods and to run the story appropriate for each test.

--- jbehave-hudson-plugin/src/test/java/org/jbehave/hudson/ReportTransformBehaviour.java
+++ jbehave-hudson-plugin/src/test/java/org/jbehave/hudson/ReportTransformBehaviour.java
@@ -1,13 +1,7 @@
 package org.jbehave.hudson;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -27,189 +21,169 @@ import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.failures.SilentlyAbsorbingFailure;
 import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.MarkUnmatchedStepsAsPending;
 import org.jbehave.core.steps.StepFinder;
 import org.jbehave.core.steps.StepFinder.ByLevenshteinDistance;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class ReportTransformBehaviour {
-	private static final String REPORT_PATH = "target/jbehave/";
-	
-	private static final String TESTSUITE_TESTCASE_FAILURE = "count(//testcase/failure)";
-	private static final String TESTSUITE_TESTCASE_IGNORED = "count(//testcase/skipped)";
-	private static final String TESTSUITE_SKIPPED_ATTRIBUTE = "/testsuite/@skipped";
-	private static final String TESTSUITE_TOTAL_TESTS_ATTRIBUTE = "/testsuite/@tests";
-	private static final String TESTSUITE_NAME_ATTRIBUTE = "/testsuite/@name";
-	private static final String TESTSUITE_FAILURE_ATTRIBUTE = "/testsuite/@failures";
-	private static final String TESTSUITE_TESTCASE_COUNT = "count(//testcase)";
+    private static final String TESTCASE_COUNT = "count(//testcase)";
+    private static final String TESTCASE_FAILURE = "count(//testcase/failure)";
+    private static final String TESTCASE_IGNORED = "count(//testcase/skipped)";
+    private static final String TESTSUITE_SKIPPED_ATTRIBUTE = "/testsuite/@skipped";
+    private static final String TESTSUITE_TESTS_ATTRIBUTE = "/testsuite/@tests";
+    private static final String TESTSUITE_NAME_ATTRIBUTE = "/testsuite/@name";
+    private static final String TESTSUITE_FAILURES_ATTRIBUTE = "/testsuite/@failures";
 
-	private static XpathEngine eng = XMLUnit.newXpathEngine();
+    private static XpathEngine engine = XMLUnit.newXpathEngine();
+    // Might be running inside IDEA or Eclipse. Can't assume current directory.
+    private File cd = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile())
+            .getParentFile().getParentFile();
 
-	@BeforeClass
-	public static void createTestReports() {
+    @Test
+    public void transformSuccessfulStoryReport() throws Throwable {
+        runStories("all_successful.story");
+        Document document = tranformReport("all_successful.xml");
+        assertEquals("0", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("3", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+    }
 
-        StoryReporterBuilder storyReporterBuilder = new StoryReporterBuilder()
-                .withDefaultFormats()
+    @Test
+    public void transformStoryReportWithNoTitle() throws Throwable {
+        runStories("all_successful.story");
+        Document document = tranformReport("all_successful.xml");
+        assertFalse("Test suite name is empty", engine.evaluate(TESTSUITE_NAME_ATTRIBUTE, document).isEmpty());
+    }
+
+    @Test
+    public void transformFailedStoryReport() throws FileNotFoundException, Throwable {
+        runStories("all_failed.story");
+        Document document = tranformReport("all_failed.xml");
+
+        assertEquals("1", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+    }
+
+    @Test
+    public void transformStoryWithExamplesReport() throws FileNotFoundException, Throwable {
+        runStories("examples_table_with_failure.story");
+        Document document = tranformReport("examples_table_with_failure.xml");
+
+        assertEquals("6", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("3", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("6", engine.evaluate(TESTCASE_COUNT, document));
+        assertEquals("3", engine.evaluate(TESTCASE_FAILURE, document));
+        assertEquals("1", engine.evaluate(TESTCASE_IGNORED, document));
+    }
+
+    @Test
+    public void transformGivenStoryReport() throws FileNotFoundException, Throwable {
+        runStories("given_story.story");
+        Document document = tranformReport("given_story.xml");
+
+        assertEquals("0", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTCASE_COUNT, document));
+    }
+
+    @Test
+    public void transformFailedGivenStoryReport() throws FileNotFoundException, Throwable {
+        runStories("given_failing_story.story");
+        Document document = tranformReport("given_failing_story.xml");
+
+        assertEquals("1", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("1", engine.evaluate(TESTCASE_COUNT, document));
+        assertEquals("1", engine.evaluate(TESTCASE_FAILURE, document));
+    }
+
+    @Test
+    public void transformFailureFollowedByGivenStoryReport() throws FileNotFoundException, Throwable {
+        runStories("failure_followed_by_given_story.story");
+        Document document = tranformReport("failure_followed_by_given_story.xml");
+
+        assertEquals("1", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("2", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("2", engine.evaluate(TESTCASE_COUNT, document));
+        assertEquals("1", engine.evaluate(TESTCASE_FAILURE, document));
+    }
+
+    @Test
+    public void transformFilterScenarioReport() throws FileNotFoundException, Throwable {
+        runStories("filter_scenario.story");
+        Document document = tranformReport("filter_scenario.xml");
+
+        assertEquals("1", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("2", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("2", engine.evaluate(TESTCASE_COUNT, document));
+        assertEquals("1", engine.evaluate(TESTCASE_FAILURE, document));
+    }
+
+    @Test
+    public void transformFilterStoryReport() throws FileNotFoundException, Throwable {
+        runStories("filter_story.story");
+        Document document = tranformReport("filter_story.xml");
+
+        assertEquals("0", engine.evaluate(TESTSUITE_FAILURES_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_TESTS_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
+        assertEquals("0", engine.evaluate(TESTCASE_COUNT, document));
+        assertEquals("0", engine.evaluate(TESTCASE_FAILURE, document));
+    }
+
+    private void runStories(String... storyPaths) {
+        StoryReporterBuilder storyReporterBuilder = new StoryReporterBuilder().withDefaultFormats()
                 .withCodeLocation(CodeLocations.codeLocationFromClass(ReportTransformBehaviour.class))
                 .withFormats(Format.XML);
 
-        Configuration conf = new MostUsefulConfiguration()
-                .useStoryReporterBuilder(storyReporterBuilder)
+        Configuration configuration = new MostUsefulConfiguration()
+                .useStoryLoader(new LoadFromClasspath(this.getClass())).useStoryReporterBuilder(storyReporterBuilder)
                 .useFailureStrategy(new SilentlyAbsorbingFailure())
                 .useStepCollector(new MarkUnmatchedStepsAsPending(new StepFinder(new ByLevenshteinDistance())));
 
+        Embedder embedder = new Embedder();
+        embedder.useConfiguration(configuration);
+        embedder.useCandidateSteps(new InstanceStepsFactory(configuration, new MySteps()).createCandidateSteps());
+        embedder.useMetaFilters(asList("-skip true"));
 
-		Embedder embedder = new Embedder() {
-
-            @Override
-			public List<CandidateSteps> candidateSteps() {
-				return new InstanceStepsFactory(configuration(), new MySteps())
-						.createCandidateSteps();
-			}
-		};
-
-        embedder.useConfiguration(conf);
-		
-		embedder.useMetaFilters(Arrays.asList("-skip true"));
-
-		ArrayList<String> storyPaths = new ArrayList<String>();
-		storyPaths.add("all_successful.story");
-		storyPaths.add("all_failed.story");
-		storyPaths.add("examples_table_with_failure.story");
-		storyPaths.add("given_story.story");
-		storyPaths.add("given_failing_story.story");
-		storyPaths.add("failure_followed_by_given_story.story");
-		storyPaths.add("filter_scenario.story");
-		storyPaths.add("filter_story.story");
-
-		try {
-			embedder.runStoriesAsPaths(storyPaths);
-		} catch (Exception e) {
+        try {
+            embedder.runStoriesAsPaths(asList(storyPaths));
+        } catch (Exception e) {
             e.printStackTrace();
-		}
-	}
+        }
+    }
 
-	
-	@Test
-	public void transformSuccessfulStoryReport() throws Throwable {
-		Document document = tranformReport(REPORT_PATH + "all_successful.xml");
+    private Document tranformReport(String path) throws TransformerFactoryConfigurationError,
+            TransformerConfigurationException, TransformerException {
 
-		assertEquals("0", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("3", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-	}
-	
-	@Test
-	public void transformStoryReportWithNoTitle() throws Throwable {
-		Document document = tranformReport(REPORT_PATH + "all_successful.xml");	
-		assertFalse("Test suite name is empty", eng.evaluate(TESTSUITE_NAME_ATTRIBUTE, document).isEmpty());
-	}
+        File report = new File(cd, "/target/jbehave/" + path);
+        Source xml = new StreamSource(report);
+        Source xslt = new StreamSource(new File(cd,
+                "src/main/resources/org/jbehave/hudson/jbehave-3.2-to-junit-1.0.xsl"));
 
-	@Test
-	public void transformFailedStoryReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "all_failed.xml");
+        Result resultOutput = new StreamResult(System.out);
+        DOMResult result = new DOMResult();
 
-		assertEquals("1", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-	}
+        Transformer transformer = TransformerFactory.newInstance().newTransformer(xslt);
+        transformer.transform(xml, result);
+        transformer.transform(xml, resultOutput);
+        return (Document) result.getNode();
+    }
 
-	@Test
-	public void transformStoryWithExamplesReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "examples_table_with_failure.xml");
-		
-		assertEquals("6", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("3", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("6", eng.evaluate(TESTSUITE_TESTCASE_COUNT , document));
-		assertEquals("3", eng.evaluate(TESTSUITE_TESTCASE_FAILURE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_IGNORED, document));
-	}
-	
-	@Test
-	public void transformGivenStoryReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "given_story.xml");
-
-		assertEquals("0", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_COUNT, document));		
-	}
-	
-	@Test
-	public void transformFailedGivenStoryReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "given_failing_story.xml");
-
-		assertEquals("1", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_COUNT, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_FAILURE, document));
-	}
-	
-	@Test
-	public void transformFailureFollowedByGivenStoryReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "failure_followed_by_given_story.xml");
-
-		assertEquals("1", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("2", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("2", eng.evaluate(TESTSUITE_TESTCASE_COUNT, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_FAILURE, document));
-	}
-
-	@Test
-	public void transformFilterScenarioReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "filter_scenario.xml");
-
-		assertEquals("1", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("2", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("2", eng.evaluate(TESTSUITE_TESTCASE_COUNT, document));
-		assertEquals("1", eng.evaluate(TESTSUITE_TESTCASE_FAILURE, document));
-	}
-	
-	@Test
-	public void transformFilterStoryReport() throws FileNotFoundException, Throwable {
-		Document document = tranformReport(REPORT_PATH + "filter_story.xml");
-
-		assertEquals("0", eng.evaluate(TESTSUITE_FAILURE_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_TOTAL_TESTS_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_SKIPPED_ATTRIBUTE, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_TESTCASE_COUNT, document));
-		assertEquals("0", eng.evaluate(TESTSUITE_TESTCASE_FAILURE, document));
-	}
-	
-	private Document tranformReport(String reportFile)
-			throws TransformerFactoryConfigurationError,
-			TransformerConfigurationException, TransformerException {
-
-        // Might be running inside IDEA or Eclipse. Can't assume current directory.
-        File cd = new File(this.getClass().getProtectionDomain()
-                .getCodeSource().getLocation().getFile())
-                .getParentFile().getParentFile();
-
-        File file = new File(cd, reportFile);
-        Source xmlSource = new StreamSource(file);
-        File file1 = new File(cd, "src/main/resources/org/jbehave/hudson/jbehave-3.2-to-junit-1.0.xsl");
-        Source xsltSource = new StreamSource(file1);
-
-		Result resultOutput = new StreamResult(System.out);
-		DOMResult result = new DOMResult();
-
-		TransformerFactory transFact = TransformerFactory.newInstance();
-
-		Transformer trans;
-		trans = transFact.newTransformer(xsltSource);
-		trans.transform(xmlSource, result);
-		trans.transform(xmlSource, resultOutput);
-		return (Document) result.getNode();
-	}
-	
 }
