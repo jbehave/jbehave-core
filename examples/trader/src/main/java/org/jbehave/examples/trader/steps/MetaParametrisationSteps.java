7654d689909f954c1bd38851685729c58535c74d
Paul Hammant <paul@hammant.org>
2011-04-20 06:22:50 -0500

    JBEHAVE-487: expand example to show alternate use

--- examples/trader/src/main/java/org/jbehave/examples/trader/steps/MetaParametrisationSteps.java
+++ examples/trader/src/main/java/org/jbehave/examples/trader/steps/MetaParametrisationSteps.java
@@ -1,9 +1,12 @@
 package org.jbehave.examples.trader.steps;
 
+import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.junit.Assert;
+
+import static junit.framework.Assert.assertEquals;
 
 
 public class MetaParametrisationSteps {
@@ -32,6 +35,12 @@ public class MetaParametrisationSteps {
         Assert.assertEquals(theme, this.theme);
         Assert.assertEquals(variant, this.variant);        
     }
+
+    // Is ignored by JBehave presently.  Perhaps because of parameter alone.
+    // @BeforeScenario
+    // public void beforeScenario(@Named("theme") String theme) {
+    //     assertEquals("parameters", theme);
+    // }
 
 
 }
