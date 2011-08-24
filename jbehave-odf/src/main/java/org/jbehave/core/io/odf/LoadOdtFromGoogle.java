49ec9aa86b6b3803f29be59309bf1dc614017064
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-08-24 08:15:13 +0200

    JBEHAVE-584:  Applied patch from Bart De Neuter using DocumentQuery API.

--- jbehave-odf/src/main/java/org/jbehave/core/io/odf/LoadOdtFromGoogle.java
+++ jbehave-odf/src/main/java/org/jbehave/core/io/odf/LoadOdtFromGoogle.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.google.gdata.client.DocumentQuery;
 import com.google.gdata.client.docs.DocsService;
 import com.google.gdata.data.MediaContent;
 import com.google.gdata.data.docs.DocumentListEntry;
@@ -15,38 +16,44 @@ import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 
 public class LoadOdtFromGoogle extends LoadOdtFromURL {
-	
-	private final DocsService client;
 
-	public LoadOdtFromGoogle(String username, String password) {
-		try {
-		    client = new DocsService("jbehave");
-			client.setUserCredentials(username, password);
-		} catch (AuthenticationException e) {
-			throw new GoogleAccessFailed(username, e);
-		}
-	}
+    private static final String DEFAULT_FEED_URI = "https://docs.google.com/feeds/default/private/full/";
+    private final DocsService client;
+    private final String feedURI;
 
-	protected InputStream resourceAsStream(String title) throws IOException, MalformedURLException {
-		try {
+    public LoadOdtFromGoogle(String username, String password) {
+        this(username, password, DEFAULT_FEED_URI);
+    }
+
+    public LoadOdtFromGoogle(String username, String password, String feedURI) {
+        try {
+            client = new DocsService("jbehave");
+            client.setUserCredentials(username, password);
+        } catch (AuthenticationException e) {
+            throw new GoogleAccessFailed(username, e);
+        }
+        this.feedURI = feedURI;
+    }
+
+    protected InputStream resourceAsStream(String title) throws IOException, MalformedURLException {
+        try {
             return documentAsStream(exportURL(title));
         } catch (ServiceException e) {
             throw new IOException(e);
         }
-	}
+    }
 
     private String exportURL(String title) throws IOException, ServiceException, MalformedURLException {
-        URL documentURL = documentURL(title);
-        List<DocumentListEntry> entries = client.getFeed(documentURL, DocumentListFeed.class).getEntries();
-        if ( entries.isEmpty() ){
-            throw new GoogleDocumentNotFound(documentURL);
+        DocumentQuery query = new DocumentQuery(new URL(feedURI));
+        query.setTitleQuery(title);
+        query.setTitleExact(true);
+        query.setMaxResults(1);
+        List<DocumentListEntry> entries = client.getFeed(query, DocumentListFeed.class).getEntries();
+        if (entries.isEmpty()) {
+            throw new GoogleDocumentNotFound(title);
         }
         return ((MediaContent) entries.get(0).getContent()).getUri() + "&exportFormat=odt";
     }
-
-	protected URL documentURL(String title) throws MalformedURLException {
-		return new URL("https://docs.google.com/feeds/default/private/full/-/document?title=" + title);
-	}
 
     private InputStream documentAsStream(String url) throws IOException, MalformedURLException {
         try {
@@ -58,32 +65,32 @@ public class LoadOdtFromGoogle extends LoadOdtFromURL {
         } catch (ServiceException e) {
             throw new GoogleMediaExportFailed(url, e);
         }
-	}
+    }
 
     @SuppressWarnings("serial")
     public static class GoogleAccessFailed extends RuntimeException {
 
         public GoogleAccessFailed(String username, Throwable cause) {
-            super("Google access failed for user "+username, cause);
+            super("Google access failed for user " + username, cause);
         }
-        
+
     }
-    
+
     @SuppressWarnings("serial")
     public static class GoogleDocumentNotFound extends RuntimeException {
 
-        public GoogleDocumentNotFound(URL url) {
-            super("Failed to find Google document from "+url);
+        public GoogleDocumentNotFound(String title) {
+            super("Failed to find Google document from " + title);
         }
-        
+
     }
 
-	@SuppressWarnings("serial")
+    @SuppressWarnings("serial")
     public static class GoogleMediaExportFailed extends RuntimeException {
 
         public GoogleMediaExportFailed(String url, Throwable cause) {
-            super("Failed to export Google media from "+url, cause);
+            super("Failed to export Google media from " + url, cause);
         }
-	    
-	}
+
+    }
 }
