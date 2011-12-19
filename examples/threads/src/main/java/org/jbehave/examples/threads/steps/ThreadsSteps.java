d2420ebd28a6a41b7c603c0646f7af10598b94c7
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-12-19 11:37:35 +0100

    JBEHAVE-563:  RunContext now throws InterruptedException if story has been cancelled.

--- examples/threads/src/main/java/org/jbehave/examples/threads/steps/ThreadsSteps.java
+++ examples/threads/src/main/java/org/jbehave/examples/threads/steps/ThreadsSteps.java
@@ -8,21 +8,27 @@ import org.jbehave.core.annotations.When;
 public class ThreadsSteps {
 
     @When("$name counts to $n Mississippi")
-    public void whenSomeoneCountsMississippis(String name, AtomicInteger n) throws InterruptedException {
+    public void whenSomeoneCountsMississippis(String name, AtomicInteger n) {
         long start = System.currentTimeMillis();
-        System.out.println(name +" starts counting to "+n);
+        System.out.println(name + " starts counting to " + n);
         for (int i = 0; i < n.intValue(); i++) {
-            System.out.println(name + " says " + i + " Mississippi (" + (System.currentTimeMillis() - start) + " millis)");
+            long elapsed = System.currentTimeMillis() - start;
+            System.out.println(name + " says " + i + " Mississippi (after " + elapsed + " millis)");
             sleepFor(1, TimeUnit.SECONDS);
         }
     }
 
-    private void sleepFor(int i, TimeUnit unit) throws InterruptedException {
-        unit.sleep(i);
+    private void sleepFor(int i, TimeUnit unit) {
+        try {
+            unit.sleep(i);
+        } catch (InterruptedException e) {
+            System.out.println("Yawn! Who's there?");
+            throw new RuntimeException(e);
+        }
     }
 
     @When("something bad happens")
-    public void whenSomethingBadHappens(){
+    public void whenSomethingBadHappens() {
         throw new RuntimeException("C'est la vie");
     }
 }
