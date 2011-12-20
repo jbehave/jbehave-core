5cd5b4d75a4cd9b7cf7cf350c069b3610da23dd2
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-12-20 16:51:39 +0100

    JBEHAVE-681:  Removed additional chars not allowed in Java.

--- jbehave-core/src/main/java/org/jbehave/core/steps/PendingStepMethodGenerator.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/PendingStepMethodGenerator.java
@@ -8,10 +8,11 @@ import org.jbehave.core.configuration.Keywords;
 import org.jbehave.core.steps.StepCreator.PendingStep;
 
 import static java.text.MessageFormat.format;
-import static java.util.Arrays.asList;
 
 public class PendingStepMethodGenerator {
 
+    private static final String[] REMOVABLES = new String[] { " ", "\'", "\"", "\\.", "\\,", "\\;", "\\:", "\\!",
+            "\\|", "<", ">", "\\*", "\\$", "\\\\", "\\/", "\\(", "\\)", "\\{", "\\}", "\\[", "\\]" };
     private static final String METHOD_SOURCE = "@{0}(\"{1}\")\n@{2}\npublic void {3}()'{'\n  // {4}\n'}'\n";
 
     private final Keywords keywords;
@@ -20,11 +21,11 @@ public class PendingStepMethodGenerator {
         this.keywords = keywords;
     }
 
-    public String generateMethod(PendingStep step) {    
+    public String generateMethod(PendingStep step) {
         String stepAsString = step.stepAsString();
         String previousNonAndStepAsString = step.previousNonAndStepAsString();
         StepType stepType = null;
-        if ( keywords.isAndStep(stepAsString) && previousNonAndStepAsString != null) {
+        if (keywords.isAndStep(stepAsString) && previousNonAndStepAsString != null) {
             stepType = keywords.stepTypeFor(previousNonAndStepAsString);
         } else {
             stepType = keywords.stepTypeFor(stepAsString);
@@ -33,16 +34,16 @@ public class PendingStepMethodGenerator {
         String stepAnnotation = StringUtils.capitalize(stepType.name().toLowerCase());
         String methodName = methodName(stepType, stepPattern);
         String pendingAnnotation = Pending.class.getSimpleName();
-        return format(METHOD_SOURCE, stepAnnotation, StringEscapeUtils.escapeJava(stepPattern), pendingAnnotation, methodName, keywords.pending());
+        return format(METHOD_SOURCE, stepAnnotation, StringEscapeUtils.escapeJava(stepPattern), pendingAnnotation,
+                methodName, keywords.pending());
     }
 
     private String methodName(StepType stepType, String stepPattern) {
         String name = stepType.name().toLowerCase() + WordUtils.capitalize(stepPattern);
-        for (String remove : asList(" ", "\'", "\"", "\\.", "\\,", "\\;", "\\:", "\\!", "\\|", "<", ">", "\\*")) {
+        for (String remove : REMOVABLES) {
             name = name.replaceAll(remove, "");
         }
         return name;
     }
-
 
 }
