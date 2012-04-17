958187acbc02c7ef9a0cd851ea7a30225bac145a
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-04-17 18:37:18 +0200

    JBEHAVE-767:  Added keywords_no.properties, contributed by Anders Sandberg Nordbø.

--- jbehave-core/src/test/java/org/jbehave/core/i18n/LocalizedKeywordsBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/i18n/LocalizedKeywordsBehaviour.java
@@ -52,6 +52,7 @@ public class LocalizedKeywordsBehaviour {
         ensureKeywordsAreLocalisedFor(new Locale("en"));
         ensureKeywordsAreLocalisedFor(new Locale("fr"));
         ensureKeywordsAreLocalisedFor(new Locale("it"));
+        ensureKeywordsAreLocalisedFor(new Locale("no"));
         ensureKeywordsAreLocalisedFor(new Locale("pt"));
         ensureKeywordsAreLocalisedFor(new Locale("tr"));
         ensureKeywordsAreLocalisedFor(new Locale("zh_TW"));
