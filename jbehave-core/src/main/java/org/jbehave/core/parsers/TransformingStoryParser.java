c40438f522a3e779326389f4c2d5642b69768dc4
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-09-12 19:35:00 +0100

    JBEHAVE-834:  Added TransformingStoryParser which parses story after having applied configured StoryTransformers.

---
+++ jbehave-core/src/main/java/org/jbehave/core/parsers/TransformingStoryParser.java
@@ -0,0 +1,31 @@
+package org.jbehave.core.parsers;
+
+import org.jbehave.core.model.Story;
+
+public class TransformingStoryParser implements StoryParser {
+
+	private final StoryParser delegate;
+	private StoryTransformer[] transformers; 
+
+	public TransformingStoryParser(StoryParser delegate, StoryTransformer... transformers) {
+		this.delegate = delegate;
+		this.transformers = transformers;
+	}
+
+	public Story parseStory(String storyAsText) {
+		return delegate.parseStory(transform(storyAsText));
+	}
+
+	public Story parseStory(String storyAsText, String storyPath) {
+		return delegate.parseStory(transform(storyAsText), storyPath);
+	}
+
+	private String transform(String storyAsText) {
+		String transformed = storyAsText;
+		for ( StoryTransformer transformer : transformers ){
+			transformed = transformer.transform(transformed);
+		}
+		return transformed;
+	}
+
+}
---
+++ jbehave-core/src/test/java/org/jbehave/core/parsers/TransformingStoryParserBehaviour.java
@@ -0,0 +1,39 @@
+package org.jbehave.core.parsers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.jbehave.core.model.Story;
+import org.junit.Test;
+
+
+public class TransformingStoryParserBehaviour {
+
+    private static final String NL = "\n";
+
+    @Test
+    public void shouldTransformAndParseStory() {
+        StoryParser delegate = new RegexStoryParser();
+        StoryTransformer transformer = new StoryTransformer() {			
+			public String transform(String storyAsText) {
+				return storyAsText.replaceAll(",", "|");
+			}
+		};
+        StoryParser parser = new TransformingStoryParser(delegate, transformer);
+        String storyAsText = "Scenario: a scenario " + NL +
+                "Given a scenario Given" + NL +
+                "When I parse it to When" + NL +
+                "And I parse it to And" + NL +
+                "!-- And ignore me too" + NL +
+                "Then I should get steps Then" + NL +
+                "Examples:" + NL +
+                ",Given,When,Then,And," + NL +
+                ",Dato che,Quando,Allora,E,";
+
+        Story story = parser.parseStory(storyAsText);
+        assertThat(story.getScenarios().get(0).getExamplesTable().getRowCount(), equalTo(1));
+    }
+
+   
+
+}
