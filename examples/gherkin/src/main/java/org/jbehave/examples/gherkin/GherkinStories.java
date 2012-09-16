7d4b8c4ba30ae698808b5ef0647aee025cb8cb60
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-09-16 10:23:40 +0100

    JBEHAVE-835:  Added gherkin example.

---
+++ examples/gherkin/src/main/java/org/jbehave/examples/gherkin/GherkinStories.java
@@ -0,0 +1,48 @@
+package org.jbehave.examples.gherkin;
+
+import java.util.List;
+
+import org.jbehave.core.Embeddable;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.parsers.gherkin.GherkinStoryParser;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.jbehave.examples.gherkin.steps.GherkinSteps;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.core.reporters.Format.HTML;
+
+public class GherkinStories extends JUnitStories {
+
+    @Override
+    public Configuration configuration() {
+        Class<? extends Embeddable> embeddableClass = this.getClass();
+        return new MostUsefulConfiguration()
+        	.useStoryParser(new GherkinStoryParser())
+            .useStoryLoader(new LoadFromClasspath(embeddableClass))
+            .useStoryReporterBuilder(new StoryReporterBuilder()
+                .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                .withDefaultFormats()
+                .withFormats(CONSOLE, HTML)
+                .withFailureTrace(true)
+                .withFailureTraceCompression(true));
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new GherkinSteps());
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), "**/*.feature", "");
+    }
+
+}
---
+++ examples/gherkin/src/main/java/org/jbehave/examples/gherkin/features/using_gherkin.feature
@@ -0,0 +1,12 @@
+
+Feature: Hello Car
+
+Scenario: Car can drive
+
+Given I have a car
+Then I can drive them according to wheels:
+| wheels | can_drive |
+|   1    |   false   |
+|   2    |   false   |
+|   3    |   false   |
+|   4    |   true    |
