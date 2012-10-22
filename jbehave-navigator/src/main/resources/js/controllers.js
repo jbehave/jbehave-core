538f2f96eaef6059d8beed5f282d7c0f7102752c
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-10-22 09:32:31 +0200

    JBEHAVE-825:  Added popup dialog to display story details.

--- jbehave-navigator/src/main/resources/js/controllers.js
+++ jbehave-navigator/src/main/resources/js/controllers.js
@@ -8,6 +8,22 @@ function StoriesController($scope, $http) {
   });
 
   $scope.showStory = function(performable) {
-	  console.log("showing "+performable.story.path)	  
+	    console.log("showing "+performable.story.path)	  
+	    var url = performable.story.path;
+	    url = url.replace(/\//g, '.');
+	    url = url.replace(".story",".html");
+	    console.log("showing "+url)	  
+	    jQuery.FrameDialog.create({
+	        width: 1000,
+	        height: 600,
+	        closeText: 'foo',
+	        url: url,
+	        title: 'Results For ' + url,
+	        closeOnEscape: true,
+	        buttons: [
+	            {
+	            }
+	        ]
+	    });
   };
 }
