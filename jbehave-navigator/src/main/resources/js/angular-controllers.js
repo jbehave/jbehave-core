4c74dcfaace3663fdd5536cb9d5d188c28e5633b
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-11-04 11:40:53 +0100

    JBEHAVE-825:  Style cleanup.

--- jbehave-navigator/src/main/resources/js/angular-controllers.js
+++ jbehave-navigator/src/main/resources/js/angular-controllers.js
@@ -10,17 +10,16 @@ function StoriesController($scope, $http) {
   $scope.predicate = '';
   
   $scope.showStory = function(storyPath) {
-	    console.log("showing "+storyPath)	  
 	    var url = storyPath;
 	    url = url.replace(/\//g, '.');
 	    url = url.replace(".story",".html");
-	    console.log("showing "+url)	  
+	    console.log("Showing story: "+url)	  
 	    jQuery.FrameDialog.create({
 	        width: 1000,
 	        height: 600,
 	        closeText: 'foo',
 	        url: url,
-	        title: 'Results For ' + url,
+	        title: storyPath,
 	        closeOnEscape: true,
 	        buttons: [
 	            {
