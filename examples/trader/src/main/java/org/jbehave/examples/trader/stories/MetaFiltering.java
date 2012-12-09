cd9337fff21ac26ed7e18b4b7c6bb17c9296451d
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-12-09 18:53:48 +0100

    JBEHAVE-868:  Fixed regression introduced by JBEHAVE-846.

--- examples/trader/src/main/java/org/jbehave/examples/trader/stories/MetaFiltering.java
+++ examples/trader/src/main/java/org/jbehave/examples/trader/stories/MetaFiltering.java
@@ -1,7 +1,14 @@
 package org.jbehave.examples.trader.stories;
 
+import java.util.Arrays;
+
 import org.jbehave.examples.trader.TraderStory;
 
 public class MetaFiltering extends TraderStory {
+
+    public MetaFiltering() {
+        // Uncomment to set meta filter, which can also be set via Ant or Maven
+        configuredEmbedder().useMetaFilters(Arrays.asList("+run yes"));
+    }
 
 }
