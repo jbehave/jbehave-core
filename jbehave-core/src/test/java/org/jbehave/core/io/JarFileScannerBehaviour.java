73a6b0664b4848249b178a6bb0e8ffcd849f8efc
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-12-20 21:51:39 +0100

    Added StoryFinder tests for lookup in jar.  Refactored tests to use Hamcrest matchers.

--- jbehave-core/src/test/java/org/jbehave/core/io/JarFileScannerBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/io/JarFileScannerBehaviour.java
@@ -6,22 +6,25 @@ import java.net.URL;
 import org.junit.Test;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class JarFileScannerBehaviour {
 
     @Test
     public void shouldScanFileFromPath() throws IOException {
         String jarPath = "src/test/resources/stories.jar";
-        assertEquals(asList("etsy_browse.story", "etsy_cart.story"),
-                new JarFileScanner(jarPath, "**/*.story", "**/*_search.story").scan());
+        JarFileScanner scanner = new JarFileScanner(jarPath, "**/*.story", "**/*_search.story");
+        assertThat(scanner.scan(), equalTo(asList("etsy_browse.story", "etsy_cart.story")));
     }
 
     @Test
     public void shouldScanFileFromURL() throws IOException {
         URL jarURL = CodeLocations.codeLocationFromPath("src/test/resources/stories.jar");
-        assertEquals(asList("etsy_browse.story", "etsy_cart.story"),
-                new JarFileScanner(jarURL, "**/*.story", "**/*_search.story").scan());
+        JarFileScanner scanner = new JarFileScanner(jarURL, "**/*.story", "**/*_search.story");
+        assertThat(scanner.scan(), equalTo(asList("etsy_browse.story", "etsy_cart.story")));
     }
 
 }
