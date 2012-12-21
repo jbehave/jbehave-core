b6dd08b97fbbd4d67d4c451e268673f1b78baab3
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-12-21 10:30:34 +0100

    JBEHAVE-870:  Allow for null include/exclude patterns

--- jbehave-core/src/test/java/org/jbehave/core/io/JarFileScannerBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/io/JarFileScannerBehaviour.java
@@ -1,30 +1,42 @@
 package org.jbehave.core.io;
 
 import java.io.IOException;
-import java.net.URL;
+import java.util.List;
 
 import org.junit.Test;
 
-import static java.util.Arrays.asList;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.not;
 
 public class JarFileScannerBehaviour {
 
     @Test
-    public void shouldScanFileFromPath() throws IOException {
-        String jarPath = "src/test/resources/stories.jar";
-        JarFileScanner scanner = new JarFileScanner(jarPath, "**/*.story", "**/*_search.story");
-        assertThat(scanner.scan(), equalTo(asList("etsy_browse.story", "etsy_cart.story")));
+    public void shouldScanJarFromPath() throws IOException {
+        List<String> paths = scan("src/test/resources/stories.jar", "**/*.story", "**/*_search.story");
+        assertThat(paths, hasItems("etsy_browse.story", "etsy_cart.story"));
+        assertThat(paths, not(hasItems("etsy_search.story")));
     }
 
     @Test
-    public void shouldScanFileFromURL() throws IOException {
-        URL jarURL = CodeLocations.codeLocationFromPath("src/test/resources/stories.jar");
-        JarFileScanner scanner = new JarFileScanner(jarURL, "**/*.story", "**/*_search.story");
-        assertThat(scanner.scan(), equalTo(asList("etsy_browse.story", "etsy_cart.story")));
+    public void shouldScanJarFromPathWithNoExcludes() throws IOException {
+        assertThat(scan("src/test/resources/stories.jar", "**/*.story", ""),
+                hasItems("etsy_browse.story", "etsy_cart.story", "etsy_search.story"));
+        assertThat(scan("src/test/resources/stories.jar", "**/*.story", null),
+                hasItems("etsy_browse.story", "etsy_cart.story", "etsy_search.story"));
+    }
+
+    @Test
+    public void shouldScanJarFromPathWithNoIncludes() throws IOException {
+        assertThat(scan("src/test/resources/stories.jar", "", "**/*.story"),
+                not(hasItems("etsy_browse.story", "etsy_cart.story", "etsy_search.story")));
+        assertThat(scan("src/test/resources/stories.jar", null, "**/*.story"),
+                not(hasItems("etsy_browse.story", "etsy_cart.story", "etsy_search.story")));
+    }
+
+    private List<String> scan(String jarPath, String includes, String excludes) {
+        return new JarFileScanner(jarPath, includes, excludes).scan();
     }
 
 }
