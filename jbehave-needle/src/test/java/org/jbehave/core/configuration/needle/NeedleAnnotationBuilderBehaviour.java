edbc59faa1b22ee800d588beefd1ec9cc43596c5
Jan Galinski <jan.galinski@holisticon.de>
2013-05-08 16:11:32 +0200

    changed order of FooSteps and FooStepsWithDependency to fix console test

--- jbehave-needle/src/test/java/org/jbehave/core/configuration/needle/NeedleAnnotationBuilderBehaviour.java
+++ jbehave-needle/src/test/java/org/jbehave/core/configuration/needle/NeedleAnnotationBuilderBehaviour.java
@@ -34,204 +34,251 @@ import org.junit.Test;
 
 public class NeedleAnnotationBuilderBehaviour {
 
-	@Test
-	public void shouldBuildConfigurationFromAnnotationsUsingConfigureAndGuiceConverters() {
-		AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
-				AnnotatedUsingConfigureAndNeedleConverters.class);
-		Configuration configuration = builderAnnotated.buildConfiguration();
-		assertThatCustomObjectIsConverted(configuration.parameterConverters());
-		assertThatDateIsConvertedWithFormat(configuration.parameterConverters(), new SimpleDateFormat("yyyy-MM-dd"));
-		assertThatExamplesTableIsConverted(configuration.parameterConverters());
+    @Test
+    public void shouldBuildConfigurationFromAnnotationsUsingConfigureAndGuiceConverters() {
+	final AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedUsingConfigureAndNeedleConverters.class);
+	final Configuration configuration = builderAnnotated
+		.buildConfiguration();
+	assertThatCustomObjectIsConverted(configuration.parameterConverters());
+	assertThatDateIsConvertedWithFormat(
+		configuration.parameterConverters(), new SimpleDateFormat(
+			"yyyy-MM-dd"));
+	assertThatExamplesTableIsConverted(configuration.parameterConverters());
+    }
+
+    private void assertThatCustomObjectIsConverted(
+	    final ParameterConverters parameterConverters) {
+	assertThat(((CustomObject) parameterConverters.convert("value",
+		CustomObject.class)).toString(), equalTo(new CustomObject(
+		"value").toString()));
+    }
+
+    private void assertThatDateIsConvertedWithFormat(
+	    final ParameterConverters parameterConverters,
+	    final DateFormat dateFormat) {
+	final String date = "2010-10-10";
+	try {
+	    assertThat((Date) parameterConverters.convert(date, Date.class),
+		    equalTo(dateFormat.parse(date)));
+	} catch (final ParseException e) {
+	    Assert.fail();
+	}
+    }
+
+    private void assertThatExamplesTableIsConverted(
+	    final ParameterConverters parameterConverters) {
+	final String tableAsString = "||one||two||\n" + "|1|2|";
+	final ExamplesTable table = new ExamplesTable(tableAsString);
+	assertThat(table.getHeaders(), hasItems("one", "two"));
+    }
+
+    @Test
+    public void shouldBuildDefaultConfigurationIfAnnotationOrAnnotatedValuesNotPresent() {
+	final AnnotationBuilder builderNotAnnotated = new NeedleAnnotationBuilder(
+		NotAnnotated.class);
+	assertThatConfigurationIs(builderNotAnnotated.buildConfiguration(),
+		new MostUsefulConfiguration());
+	final AnnotationBuilder builderAnnotatedWithoutModules = new NeedleAnnotationBuilder(
+		AnnotatedWithoutInjectors.class);
+	assertThatConfigurationIs(
+		builderAnnotatedWithoutModules.buildConfiguration(),
+		new MostUsefulConfiguration());
+    }
+
+    private void assertThatConfigurationIs(
+	    final Configuration builtConfiguration,
+	    final Configuration defaultConfiguration) {
+	assertThat(builtConfiguration.failureStrategy(),
+		instanceOf(defaultConfiguration.failureStrategy().getClass()));
+	assertThat(builtConfiguration.storyLoader(),
+		instanceOf(defaultConfiguration.storyLoader().getClass()));
+	assertThat(builtConfiguration.stepPatternParser(),
+		instanceOf(defaultConfiguration.stepPatternParser().getClass()));
+	assertThat(builtConfiguration.storyReporterBuilder().formats(),
+		equalTo(defaultConfiguration.storyReporterBuilder().formats()));
+	assertThat(builtConfiguration.storyReporterBuilder().outputDirectory(),
+		equalTo(defaultConfiguration.storyReporterBuilder()
+			.outputDirectory()));
+	assertThat(builtConfiguration.storyReporterBuilder().viewResources(),
+		equalTo(defaultConfiguration.storyReporterBuilder()
+			.viewResources()));
+	assertThat(builtConfiguration.storyReporterBuilder()
+		.reportFailureTrace(), equalTo(defaultConfiguration
+		.storyReporterBuilder().reportFailureTrace()));
+    }
+
+    @Test
+    public void shouldBuildCandidateStepsFromAnnotationsUsingNeedle() {
+	final AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedUsingNeedle.class);
+	final Configuration configuration = builderAnnotated
+		.buildConfiguration();
+	assertTrue(builderAnnotated.buildCandidateSteps(configuration)
+		.isEmpty());
+    }
+
+    @Test
+    public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndNeedle() {
+	final AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedUsingStepsAndNeedle.class);
+	final Configuration configuration = builderAnnotated
+		.buildConfiguration();
+	assertThatStepsInstancesAre(
+		builderAnnotated.buildCandidateSteps(configuration),
+		FooSteps.class);
+    }
+
+    @Test
+    public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndGuiceAndConverters() {
+	final AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedUsingConfigureAndNeedleConverters.class);
+	final Configuration configuration = builderAnnotated
+		.buildConfiguration();
+	assertThatStepsInstancesAre(
+		builderAnnotated.buildCandidateSteps(configuration),
+		FooSteps.class);
+    }
+
+    @Test
+    public void shouldBuildEmptyStepsListIfAnnotationOrAnnotatedValuesNotPresent() {
+	final AnnotationBuilder builderNotAnnotated = new NeedleAnnotationBuilder(
+		NotAnnotated.class);
+	assertThatStepsInstancesAre(builderNotAnnotated.buildCandidateSteps());
+
+	final AnnotationBuilder builderAnnotatedWithoutLocations = new NeedleAnnotationBuilder(
+		AnnotatedWithoutInjectors.class);
+	assertThatStepsInstancesAre(builderAnnotatedWithoutLocations
+		.buildCandidateSteps());
+    }
+
+    @Test
+    public void shouldBuildStepsList() {
+	final AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedMultipleSteps.class);
+	final List<CandidateSteps> actual = builderAnnotated
+		.buildCandidateSteps();
+	assertThatStepsInstancesAre(actual, FooStepsWithDependency.class,
+		FooSteps.class);
+    }
+
+    @Test
+    public void shouldCreateOnlyOneContainerForMultipleBuildInvocations() {
+	final NeedleAnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedUsingStepsAndNeedle.class);
+	builderAnnotated.buildConfiguration();
+	assertTrue(!builderAnnotated.getProvider().isEmpty());
+    }
+
+    @Test
+    public void shouldSupplyInjectors() {
+	final NeedleAnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
+		AnnotatedWithStepsWithDependency.class);
+	final List<CandidateSteps> buildCandidateSteps = builderAnnotated
+		.buildCandidateSteps();
+	assertThatStepsInstancesAre(buildCandidateSteps,
+		FooStepsWithDependency.class);
+	final ValueGetter getter = ((FooStepsWithDependency) ((Steps) buildCandidateSteps
+		.get(0)).instance()).getGetter();
+	assertNotNull(getter);
+	assertThat((String) getter.getValue(), is(ValueGetter.VALUE));
+    }
+
+    // TODO currently this method depends on order of elements in annotation
+    // FIXME provide sorting of array
+    private void assertThatStepsInstancesAre(
+	    final List<CandidateSteps> candidateSteps,
+	    final Class<?>... stepsClasses) {
+	assertThat(candidateSteps.size(), equalTo(stepsClasses.length));
+	for (int i = 0; i < stepsClasses.length; i++) {
+	    assertThat(((Steps) candidateSteps.get(i)).instance(),
+		    instanceOf(stepsClasses[i]));
+	}
+    }
+
+    @Configure()
+    @UsingNeedle(provider = { ValueGetterProvider.class })
+    private static class AnnotatedUsingNeedle {
+
+    }
+
+    @Configure()
+    @UsingSteps(instances = { FooSteps.class })
+    @UsingNeedle(provider = { ValueGetterProvider.class })
+    private static class AnnotatedUsingStepsAndNeedle {
+
+    }
+
+    @Configure(parameterConverters = { MyExampleTableConverter.class,
+	    MyDateConverter.class, CustomConverter.class })
+    @UsingSteps(instances = { FooSteps.class })
+    @UsingNeedle(provider = { ValueGetterProvider.class })
+    private static class AnnotatedUsingConfigureAndNeedleConverters {
+
+    }
+
+    @Configure()
+    @UsingNeedle()
+    private static class AnnotatedWithoutInjectors {
+
+    }
+
+    @Configure()
+    @UsingSteps(instances = { FooStepsWithDependency.class })
+    @UsingNeedle()
+    private static class AnnotatedWithStepsWithDependency {
+
+    }
+
+    @Configure()
+    @UsingSteps(instances = { FooStepsWithDependency.class, FooSteps.class })
+    @UsingNeedle()
+    private static class AnnotatedMultipleSteps {
+
+    }
+
+    private static class NotAnnotated {
+
+    }
+
+    public static class CustomConverter implements ParameterConverter {
+
+	public boolean accept(final Type type) {
+	    return ((Class<?>) type).isAssignableFrom(CustomObject.class);
 	}
 
-	private void assertThatCustomObjectIsConverted(ParameterConverters parameterConverters) {
-		assertThat(((CustomObject) parameterConverters.convert("value", CustomObject.class)).toString(),
-				equalTo(new CustomObject("value").toString()));
+	public Object convertValue(final String value, final Type type) {
+	    return new CustomObject(value);
+	}
+    }
+
+    public static class MyExampleTableConverter extends
+	    ParameterConverters.ExamplesTableConverter {
+
+	public MyExampleTableConverter() {
 	}
 
-	private void assertThatDateIsConvertedWithFormat(ParameterConverters parameterConverters, DateFormat dateFormat) {
-		String date = "2010-10-10";
-		try {
-			assertThat((Date) parameterConverters.convert(date, Date.class), equalTo(dateFormat.parse(date)));
-		} catch (ParseException e) {
-			Assert.fail();
-		}
+    }
+
+    public static class MyDateConverter extends
+	    ParameterConverters.DateConverter {
+
+	public MyDateConverter() {
+	    super(new SimpleDateFormat("yyyy-MM-dd"));
+	}
+    }
+
+    public static class CustomObject {
+
+	private final String value;
+
+	public CustomObject(final String value) {
+	    this.value = value;
 	}
 
-	private void assertThatExamplesTableIsConverted(ParameterConverters parameterConverters) {
-		String tableAsString = "||one||two||\n" + "|1|2|";
-		ExamplesTable table = new ExamplesTable(tableAsString);
-		assertThat(table.getHeaders(), hasItems("one", "two"));
+	@Override
+	public String toString() {
+	    return value;
 	}
-
-	@Test
-	public void shouldBuildDefaultConfigurationIfAnnotationOrAnnotatedValuesNotPresent() {
-		AnnotationBuilder builderNotAnnotated = new NeedleAnnotationBuilder(NotAnnotated.class);
-		assertThatConfigurationIs(builderNotAnnotated.buildConfiguration(), new MostUsefulConfiguration());
-		AnnotationBuilder builderAnnotatedWithoutModules = new NeedleAnnotationBuilder(AnnotatedWithoutInjectors.class);
-		assertThatConfigurationIs(builderAnnotatedWithoutModules.buildConfiguration(), new MostUsefulConfiguration());
-	}
-
-	private void assertThatConfigurationIs(Configuration builtConfiguration, Configuration defaultConfiguration) {
-		assertThat(builtConfiguration.failureStrategy(), instanceOf(defaultConfiguration.failureStrategy().getClass()));
-		assertThat(builtConfiguration.storyLoader(), instanceOf(defaultConfiguration.storyLoader().getClass()));
-		assertThat(builtConfiguration.stepPatternParser(), instanceOf(defaultConfiguration.stepPatternParser()
-				.getClass()));
-		assertThat(builtConfiguration.storyReporterBuilder().formats(), equalTo(defaultConfiguration
-				.storyReporterBuilder().formats()));
-		assertThat(builtConfiguration.storyReporterBuilder().outputDirectory(), equalTo(defaultConfiguration
-				.storyReporterBuilder().outputDirectory()));
-		assertThat(builtConfiguration.storyReporterBuilder().viewResources(), equalTo(defaultConfiguration
-				.storyReporterBuilder().viewResources()));
-		assertThat(builtConfiguration.storyReporterBuilder().reportFailureTrace(), equalTo(defaultConfiguration
-				.storyReporterBuilder().reportFailureTrace()));
-	}
-
-	@Test
-	public void shouldBuildCandidateStepsFromAnnotationsUsingNeedle() {
-		AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(AnnotatedUsingNeedle.class);
-		Configuration configuration = builderAnnotated.buildConfiguration();
-		assertTrue(builderAnnotated.buildCandidateSteps(configuration).isEmpty());
-	}
-
-	@Test
-	public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndNeedle() {
-		AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(AnnotatedUsingStepsAndNeedle.class);
-		Configuration configuration = builderAnnotated.buildConfiguration();
-		assertThatStepsInstancesAre(builderAnnotated.buildCandidateSteps(configuration), FooSteps.class);
-	}
-
-	@Test
-	public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndGuiceAndConverters() {
-		AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(
-				AnnotatedUsingConfigureAndNeedleConverters.class);
-		Configuration configuration = builderAnnotated.buildConfiguration();
-		assertThatStepsInstancesAre(builderAnnotated.buildCandidateSteps(configuration), FooSteps.class);
-	}
-
-	@Test
-	public void shouldBuildEmptyStepsListIfAnnotationOrAnnotatedValuesNotPresent() {
-		AnnotationBuilder builderNotAnnotated = new NeedleAnnotationBuilder(NotAnnotated.class);
-		assertThatStepsInstancesAre(builderNotAnnotated.buildCandidateSteps());
-
-		AnnotationBuilder builderAnnotatedWithoutLocations = new NeedleAnnotationBuilder(
-				AnnotatedWithoutInjectors.class);
-		assertThatStepsInstancesAre(builderAnnotatedWithoutLocations.buildCandidateSteps());
-	}
-
-	@Test
-	public void shouldBuildStepsList() {
-		AnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(AnnotatedMultipleSteps.class);
-		List<CandidateSteps> actual = builderAnnotated.buildCandidateSteps();
-		assertThatStepsInstancesAre(actual, FooSteps.class, FooStepsWithDependency.class);
-	}
-
-	@Test
-	public void shouldCreateOnlyOneContainerForMultipleBuildInvocations() {
-		NeedleAnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(AnnotatedUsingStepsAndNeedle.class);
-		builderAnnotated.buildConfiguration();
-		assertTrue(!builderAnnotated.getProvider().isEmpty());
-	}
-
-	@Test
-	public void shouldSupplyInjectors() {
-		NeedleAnnotationBuilder builderAnnotated = new NeedleAnnotationBuilder(AnnotatedWithStepsWithDependency.class);
-		List<CandidateSteps> buildCandidateSteps = builderAnnotated.buildCandidateSteps();
-		assertThatStepsInstancesAre(buildCandidateSteps, FooStepsWithDependency.class);
-		ValueGetter getter = ((FooStepsWithDependency) ((Steps) buildCandidateSteps.get(0)).instance()).getGetter();
-		assertNotNull(getter);
-		assertThat((String) getter.getValue(), is(ValueGetter.VALUE));
-	}
-
-	// TODO currently this method depends on order of elements in annotation
-	// FIXME provide sorting of array
-	private void assertThatStepsInstancesAre(List<CandidateSteps> candidateSteps, Class<?>... stepsClasses) {
-		assertThat(candidateSteps.size(), equalTo(stepsClasses.length));
-		for (int i = 0; i < stepsClasses.length; i++) {
-			assertThat(((Steps) candidateSteps.get(i)).instance(), instanceOf(stepsClasses[i]));
-		}
-	}
-
-	@Configure()
-	@UsingNeedle(provider = { ValueGetterProvider.class })
-	private static class AnnotatedUsingNeedle {
-
-	}
-
-	@Configure()
-	@UsingSteps(instances = { FooSteps.class })
-	@UsingNeedle(provider = { ValueGetterProvider.class })
-	private static class AnnotatedUsingStepsAndNeedle {
-
-	}
-
-	@Configure(parameterConverters = { MyExampleTableConverter.class, MyDateConverter.class, CustomConverter.class })
-	@UsingSteps(instances = { FooSteps.class })
-	@UsingNeedle(provider = { ValueGetterProvider.class })
-	private static class AnnotatedUsingConfigureAndNeedleConverters {
-
-	}
-
-	@Configure()
-	@UsingNeedle()
-	private static class AnnotatedWithoutInjectors {
-
-	}
-
-	@Configure()
-	@UsingSteps(instances = { FooStepsWithDependency.class })
-	@UsingNeedle()
-	private static class AnnotatedWithStepsWithDependency {
-
-	}
-
-	@Configure()
-	@UsingSteps(instances = { FooStepsWithDependency.class, FooSteps.class })
-	@UsingNeedle()
-	private static class AnnotatedMultipleSteps {
-
-	}
-
-	private static class NotAnnotated {
-
-	}
-
-	public static class CustomConverter implements ParameterConverter {
-
-		public boolean accept(Type type) {
-			return ((Class<?>) type).isAssignableFrom(CustomObject.class);
-		}
-
-		public Object convertValue(String value, Type type) {
-			return new CustomObject(value);
-		}
-	}
-
-	public static class MyExampleTableConverter extends ParameterConverters.ExamplesTableConverter {
-
-		public MyExampleTableConverter() {
-		}
-
-	}
-
-	public static class MyDateConverter extends ParameterConverters.DateConverter {
-
-		public MyDateConverter() {
-			super(new SimpleDateFormat("yyyy-MM-dd"));
-		}
-	}
-
-	public static class CustomObject {
-
-		private final String value;
-
-		public CustomObject(String value) {
-			this.value = value;
-		}
-
-		@Override
-		public String toString() {
-			return value;
-		}
-	}
+    }
 }
