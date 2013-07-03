85d84228bf83dcc38c346dee50cd5775c5f253f4
jangalinski <jan.galinski@holisticon.de>
2013-07-03 22:08:06 +0200

    JBEHAVE-899: Steps instance creation now supports needles constructor injection
    Conflicts:
    	jbehave-needle/src/main/java/org/jbehave/core/steps/needle/configuration/CreateInstanceByDefaultConstructor.java

--- jbehave-needle/src/main/java/org/jbehave/core/steps/needle/configuration/CreateInstanceByDefaultConstructor.java
+++ jbehave-needle/src/main/java/org/jbehave/core/steps/needle/configuration/CreateInstanceByDefaultConstructor.java
@@ -2,26 +2,23 @@ package org.jbehave.core.steps.needle.configuration;
 
 /**
  * Instantiates new java object by default constructor
- * 
  * @author Jan Galinski, Holisticon AG (jan.galinski@holisticon.de)
  * @author Simon Zambrovski, Holisticon AG (simon.zambrovski@holisticon.de)
  */
 public enum CreateInstanceByDefaultConstructor {
-	/**
-	 * Singleton
-	 */
-	INSTANCE;
 
-	public final <T> T apply(final Class<T> type) {
-		try {
-			final T newInstance = type.getConstructor().newInstance();
-			return newInstance;
-		} catch (final Exception e) {
-			throw new IllegalStateException(
-					String.format(
-							"Can not instantiate instance of %s by default constructor.",
-							type.getSimpleName()), e);
-		}
-	}
+    /**
+     * Singleton
+     */
+    INSTANCE;
+
+    public final <T> T apply(final Class<T> type) {
+        try {
+            final T newInstance = type.getConstructor().newInstance();
+            return newInstance;
+        } catch (final Exception e) {
+            throw new IllegalStateException(String.format("Can not instantiate instance of %s by default constructor.", type.getSimpleName()), e);
+        }
+    }
 
 }
