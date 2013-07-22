7e81a6f12ea3145842c7e9e8acdf4755a06dbeb0
Simon Zambrovski <simon.zambrovski@holisticon.de>
2013-07-22 23:14:53 +0200

    JBEHAVE-899: Added documentation and needle archetype

---
+++ archetypes/jbehave-needle-archetype/src/main/resources/archetype-resources/src/main/java/steps/UsualService.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.steps;
+
+public interface UsualService {
+
+	void goodSoul();
+
+	void isStillStep();
+
+	void aStep();
+}
