974816035cc0452a31aabc94db125daa059a0ef1
Mauro Talevi <mauro.talevi@aquilonia.org>
2013-11-26 00:00:32 +0000

    JBEHAVE-958:  Added maven goal to import to filesystem.  Updated docs.

---
+++ jbehave-rest/src/main/java/org/jbehave/core/io/rest/mojo/ImportToFilesystemMojo.java
@@ -0,0 +1,100 @@
+package org.jbehave.core.io.rest.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.jbehave.core.io.ResourceLoader;
+import org.jbehave.core.io.rest.ImportToFilesystem;
+import org.jbehave.core.io.rest.RESTClient.Type;
+import org.jbehave.core.io.rest.ResourceImporter;
+import org.jbehave.core.io.rest.ResourceIndexer;
+import org.jbehave.core.io.rest.redmine.IndexFromRedmine;
+import org.jbehave.core.io.rest.redmine.LoadFromRedmine;
+
+/**
+ * Mojo to import resources from REST root path to filesystem target path.
+ * 
+ * @goal import-to-filesystem
+ * @requiresProject false
+ */
+public class ImportToFilesystemMojo extends AbstractMojo {
+
+    private static final String REDMINE = "redmine";
+
+    /**
+     * The REST provider
+     * 
+     * @parameter default-value="redmine" expression="${jbehave.restProvider}
+     */
+    String restProvider;
+
+    /**
+     * The root path of the REST API
+     * 
+     * @parameter expression="${jbehave.restRootPath}
+     * @required
+     */
+    String restRootPath;
+
+    /**
+     * The username to access the REST API. May be null if no security enabled.
+     * 
+     * @parameter expression="${jbehave.restUsername}
+     */
+    String restUsername;
+
+    /**
+     * The password to access the REST API. May be null if no security enabled.
+     * 
+     * @parameter expression="${jbehave.restPassword}
+     */
+    String restPassword;
+
+    /**
+     * The target path of the filesystem to which the resources are written
+     * 
+     * @parameter default-value="target/stories"
+     *            expression="${jbehave.targetPath}
+     */
+    String targetPath;
+
+    /**
+     * The extension of the files written
+     * 
+     * @parameter default-value=".story" expression="${jbehave.targetExt}
+     */
+    String targetExt;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            getLog().info("Importing to filesystem resources from REST root path " + restRootPath);
+            ResourceImporter importer = createImporter();
+            importer.importResources(restRootPath);
+        } catch (Exception e) {
+            String message = "Failed to import to filesystem resources from REST root path " + restRootPath;
+            getLog().warn(message);
+            throw new MojoExecutionException(message, e);
+        }
+    }
+
+    private ResourceImporter createImporter() {
+        ResourceIndexer indexer = newResourceIndexer();
+        ResourceLoader loader = newResourceLoader();
+        return new ImportToFilesystem(indexer, loader, targetPath, targetExt);
+    }
+
+    ResourceIndexer newResourceIndexer() {
+        if (restProvider.equals(REDMINE)) {
+            return new IndexFromRedmine(restUsername, restPassword);
+        }
+        throw new RuntimeException("Unsupported REST provider " + restProvider);
+    }
+
+    ResourceLoader newResourceLoader() {
+        if (restProvider.equals(REDMINE)) {
+            return new LoadFromRedmine(Type.JSON, restUsername, restPassword);
+        }
+        throw new RuntimeException("Unsupported REST provider " + restProvider);
+    }
+
+}
---
+++ jbehave-rest/src/test/java/org/jbehave/core/io/rest/mojo/ImportToFilesystemMojoBehaviour.java
@@ -0,0 +1,73 @@
+package org.jbehave.core.io.rest.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.jbehave.core.io.ResourceLoader;
+import org.jbehave.core.io.rest.Resource;
+import org.jbehave.core.io.rest.ResourceIndexer;
+import org.junit.Test;
+
+import static org.apache.commons.io.FileUtils.readFileToString;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ImportToFilesystemMojoBehaviour {
+
+    @Test
+    public void canImportToFilesystem() throws IOException, MojoExecutionException, MojoFailureException {
+
+        // Given
+        final ResourceIndexer indexer = mock(ResourceIndexer.class);
+        final ResourceLoader loader = mock(ResourceLoader.class);
+        String sourcePath = "http://wiki";
+        Map<String, Resource> index = new HashMap<String, Resource>();
+        index.put("one", new Resource("one", sourcePath + "/one"));
+        index.put("two", new Resource("two", sourcePath + "/two"));
+        when(indexer.indexResources(sourcePath)).thenReturn(index);
+        String text1 = "story text 1";
+        when(loader.loadResourceAsText(index.get("one").getURI())).thenReturn(text1);
+        String text2 = "story text 2";
+        when(loader.loadResourceAsText(index.get("two").getURI())).thenReturn(text2);
+
+        // When
+        String targetPath = "target/stories";
+        String targetExt = ".story";
+        ImportToFilesystemMojo mojo = new ImportToFilesystemMojo(){
+
+            @Override
+            ResourceIndexer newResourceIndexer() {
+                return indexer;
+            }
+
+            @Override
+            ResourceLoader newResourceLoader() {
+                return loader;
+            }
+            
+        };
+        mojo.targetPath = targetPath;
+        mojo.targetExt = targetExt;
+        
+        mojo.execute();
+        
+        // Then
+        File file1 = new File(targetPath + "/one" + targetExt);
+        assertThat(file1.exists(), equalTo(true));
+        assertThat(readFileToString(file1), equalTo(text1));
+        File file2 = new File(targetPath + "/two" + targetExt);
+        assertThat(file2.exists(), equalTo(true));
+        assertThat(readFileToString(file2), equalTo(text2));
+
+    }
+
+}
