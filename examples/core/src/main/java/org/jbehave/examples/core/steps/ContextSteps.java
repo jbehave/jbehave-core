656e36e86a573df656003aceeda545b8f3306d3a
Mauro Talevi <mauro.talevi@aquilonia.org>
2013-12-28 22:02:25 +0100

    Added example context.story to ushow the use of a context object to share variables between steps.
    Conflicts:
    	examples/core/src/main/java/org/jbehave/examples/core/CoreStory.java

---
+++ examples/core/src/main/java/org/jbehave/examples/core/steps/ContextSteps.java
@@ -0,0 +1,44 @@
+package org.jbehave.examples.core.steps;
+
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.not;
+
+import java.util.Map;
+
+import org.hamcrest.MatcherAssert;
+import org.jbehave.core.annotations.AfterScenario;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+
+public class ContextSteps {
+
+	private MyContext context;
+
+	public ContextSteps(MyContext context) {
+		this.context = context;
+	}
+	
+	@When("a variable of name %name is processed")
+	public void whenIProcessAVariable(String name){
+		context.variables.put(name, name);
+	}
+
+	@Then("the context variables includes the name %name")
+	public void thenTheContextIncludes(String name){
+		Map<String, Object> variables = context.variables;
+		MatcherAssert.assertThat(variables, hasEntry(name, (Object)name));
+	}
+
+	@Then("the context variables does not include the name %name")
+	public void thenTheContextDoesNotInclude(String name){
+		Map<String, Object> variables = context.variables;
+		MatcherAssert.assertThat(variables, not(hasEntry(name, (Object)name)));
+	}
+
+	@AfterScenario
+	public void afterScenario(){
+		context.variables.clear();
+	}
+	
+}
---
+++ examples/core/src/main/java/org/jbehave/examples/core/steps/MyContext.java
@@ -0,0 +1,10 @@
+package org.jbehave.examples.core.steps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyContext {
+
+	Map<String,Object> variables = new HashMap<String, Object>();
+	
+}
