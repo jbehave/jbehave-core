fd614111ea909fcbbeae2a36ca433016b3ff3eea
Mauro Talevi <mauro.talevi@aquilonia.org>
2014-02-28 08:10:29 +0000

    JBEHAVE-789:  Cleanup.

--- examples/core/src/main/java/org/jbehave/examples/core/stories/GivenStoriesParametrisedByMeta.java
+++ examples/core/src/main/java/org/jbehave/examples/core/stories/GivenStoriesParametrisedByMeta.java
@@ -1,7 +1,17 @@
 package org.jbehave.examples.core.stories;
 
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.examples.core.CoreStory;
+import org.jbehave.examples.core.service.TradingService;
+import org.jbehave.examples.core.steps.TraderSteps;
 
 public class GivenStoriesParametrisedByMeta extends CoreStory {
 
+	@Override
+	public InjectableStepsFactory stepsFactory() {
+		return new InstanceStepsFactory(configuration(), new TraderSteps(
+				new TradingService()));
+	}
+	
 }
--- jbehave-core/src/main/java/org/jbehave/core/embedder/FilteredStory.java
+++ jbehave-core/src/main/java/org/jbehave/core/embedder/FilteredStory.java
@@ -10,37 +10,48 @@ import org.jbehave.core.model.Story;
 public class FilteredStory {
 
 	private boolean alwaysAllowed;
-    private boolean storyAllowed;
-    private Map<Scenario, Boolean> scenariosAllowed;
+	private boolean storyAllowed;
+	private Map<Scenario, Boolean> scenariosAllowed;
 
-    public FilteredStory(MetaFilter filter, Story story, StoryControls storyControls){
-    	this(filter, story, storyControls, false);
-    }
-    
-    public FilteredStory(MetaFilter filter, Story story, StoryControls storyControls, boolean givenStory) {
-    	if (  givenStory && storyControls.ignoreMetaFiltersIfGivenStory() ){
-    		alwaysAllowed = true;
-    	}
-        String storyMetaPrefix = storyControls.storyMetaPrefix();
-        String scenarioMetaPrefix = storyControls.scenarioMetaPrefix();
-        Meta storyMeta = story.getMeta().inheritFrom(story.asMeta(storyMetaPrefix));
-        storyAllowed = filter.allow(storyMeta);
-        scenariosAllowed = new HashMap<Scenario, Boolean>();
-        for (Scenario scenario : story.getScenarios()) {
-            Meta scenarioMeta = scenario.getMeta().inheritFrom(
-                    scenario.asMeta(scenarioMetaPrefix).inheritFrom(storyMeta));
-            boolean scenarioAllowed = filter.allow(scenarioMeta);
-            scenariosAllowed.put(scenario, scenarioAllowed);
-        }
-    }
+	public FilteredStory(MetaFilter filter, Story story,
+			StoryControls storyControls) {
+		this(filter, story, storyControls, false);
+	}
 
-    public boolean allowed() {
-    	if ( alwaysAllowed ) return true;
-        return storyAllowed || scenariosAllowed.values().contains(true);
-    }
+	public FilteredStory(MetaFilter filter, Story story,
+			StoryControls storyControls, boolean givenStory) {
+		if (givenStory && storyControls.ignoreMetaFiltersIfGivenStory()) {
+			alwaysAllowed = true;
+		} else {
+			String storyMetaPrefix = storyControls.storyMetaPrefix();
+			String scenarioMetaPrefix = storyControls.scenarioMetaPrefix();
+			Meta storyMeta = story.getMeta().inheritFrom(
+					story.asMeta(storyMetaPrefix));
+			storyAllowed = filter.allow(storyMeta);
+			scenariosAllowed = new HashMap<Scenario, Boolean>();
+			for (Scenario scenario : story.getScenarios()) {
+				Meta scenarioMeta = scenario.getMeta().inheritFrom(
+						scenario.asMeta(scenarioMetaPrefix).inheritFrom(
+								storyMeta));
+				boolean scenarioAllowed = filter.allow(scenarioMeta);
+				scenariosAllowed.put(scenario, scenarioAllowed);
+			}
+		}
+	}
 
-    public boolean allowed(Scenario scenario) {
-    	if ( alwaysAllowed ) return true;
-        return scenariosAllowed.get(scenario);
-    }
+	public boolean allowed() {
+		if (alwaysAllowed) {
+			return true;
+		} else {
+			return storyAllowed || scenariosAllowed.values().contains(true);
+		}
+	}
+
+	public boolean allowed(Scenario scenario) {
+		if (alwaysAllowed) {
+			return true;
+		} else {
+			return scenariosAllowed.get(scenario);
+		}
+	}
 }
