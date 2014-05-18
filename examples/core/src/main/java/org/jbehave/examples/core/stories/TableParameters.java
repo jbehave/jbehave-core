4aaa7c6782cf44a6c43ca0f2cb0e83aa52b38c31
Mauro Talevi <mauro.talevi@aquilonia.org>
2014-05-18 19:13:32 +0200

    JBEHAVE-1018:  Fixed marking of parameters if there is more than one parameter and a table.

---
+++ examples/core/src/main/java/org/jbehave/examples/core/stories/TableParameters.java
@@ -0,0 +1,14 @@
+package org.jbehave.examples.core.stories;
+
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.jbehave.examples.core.CoreStory;
+import org.jbehave.examples.core.steps.TableSteps;
+
+public class TableParameters extends CoreStory {
+
+	@Override
+	public InjectableStepsFactory stepsFactory() {
+		return new InstanceStepsFactory(configuration(), new TableSteps());
+	}
+}
--- jbehave-core/src/main/java/org/jbehave/core/steps/StepCreator.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/StepCreator.java
@@ -220,8 +220,9 @@ public class StepCreator {
             ParameterName[] names, String[] parameterValues) {
     	String parametrisedStep = stepAsString;
     	// mark parameter values that are parsed
+    	boolean hasTable = hasTable(types);
         for (int position = 0; position < types.length; position++) {
-            parametrisedStep = markParsedParameterValue(parametrisedStep, types[position], parameterValues[position]);
+            parametrisedStep = markParsedParameterValue(parametrisedStep, types[position], parameterValues[position], hasTable);
         }
         // mark parameter values that are named
         for (String name : namedParameters.keySet()) {
@@ -231,7 +232,16 @@ public class StepCreator {
         return parametrisedStep;
     }
 
-    private String markNamedParameterValue(String stepText, Map<String, String> namedParameters, String name) {
+    private boolean hasTable(Type[] types) {
+    	for (Type type : types) {
+			if ( isTable(type) ){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String markNamedParameterValue(String stepText, Map<String, String> namedParameters, String name) {
         String value = namedParameter(namedParameters, name);
         if (value != null) {
 			stepText = stepText.replace(delimitedName(name), markedValue(value));
@@ -243,7 +253,7 @@ public class StepCreator {
 		return parameterControls.nameDelimiterLeft() + name + parameterControls.nameDelimiterRight();
 	}
 
-    private String markParsedParameterValue(String stepText, Type type, String value) {
+    private String markParsedParameterValue(String stepText, Type type, String value, boolean hasTable) {
         if (value != null) {
             if (isTable(type)) {
                 stepText = stepText.replace(value, markedTable(value));
@@ -256,7 +266,9 @@ public class StepCreator {
                 	String rightPad = ( stepText.endsWith(value) ? NONE : SPACE );
             		stepText = stepText.replace(pad(value, leftPad, rightPad), pad(markedValue, leftPad, rightPad));
                 }
-                stepText = stepText.replace(NEWLINE, PARAMETER_VALUE_NEWLINE);
+                if ( !hasTable ){
+                	stepText = stepText.replace(NEWLINE, PARAMETER_VALUE_NEWLINE);
+                }
             }
         }
         return stepText;
