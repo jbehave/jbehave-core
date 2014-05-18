6599ed814229f879df240218bc88483a81c86398
Simon Zambrovski <simon.zambrovski@holisticon.de>
2014-05-18 15:44:56 +0200

    JBEHAVE-1017 Switched to Needle4J 2.3

--- examples/needle/src/main/java/org/jbehave/examples/core/needle/CoreStoriesUsingNeedle.java
+++ examples/needle/src/main/java/org/jbehave/examples/core/needle/CoreStoriesUsingNeedle.java
@@ -1,5 +1,7 @@
 package org.jbehave.examples.core.needle;
 
+import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,8 +10,8 @@ import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.needle.NeedleStepsFactory;
 import org.jbehave.examples.core.CoreStories;
-import org.jbehave.examples.core.needle.provider.TraderServiceInjectionProvider;
 import org.jbehave.examples.core.needle.steps.NeedleTraderSteps;
+import org.jbehave.examples.core.service.TradingService;
 import org.jbehave.examples.core.steps.AndSteps;
 import org.jbehave.examples.core.steps.BeforeAfterSteps;
 import org.jbehave.examples.core.steps.CalendarSteps;
@@ -17,9 +19,8 @@ import org.jbehave.examples.core.steps.PendingSteps;
 import org.jbehave.examples.core.steps.PriorityMatchingSteps;
 import org.jbehave.examples.core.steps.SandpitSteps;
 import org.jbehave.examples.core.steps.SearchSteps;
-
-import de.akquinet.jbosscc.needle.injection.InjectionProvider;
-import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
+import org.needle4j.injection.InjectionProvider;
+import org.needle4j.injection.InjectionProviders;
 
 /**
  * Run core stories using NeedleStepsFactory. The textual trader stories are
@@ -34,7 +35,7 @@ public class CoreStoriesUsingNeedle extends CoreStories {
         PriorityMatchingSteps.class, SandpitSteps.class, SearchSteps.class };
 
     final Set<InjectionProvider<?>> providers = new HashSet<InjectionProvider<?>>();
-    providers.add(new TraderServiceInjectionProvider());
+    providers.add(InjectionProviders.providerForInstance(new TradingService()));
 
     return new NeedleStepsFactory(configuration(), providers, steps);
   }
--- examples/needle/src/main/java/org/jbehave/examples/core/needle/ParentAnnotatedEmbedderUsingNeedle.java
+++ examples/needle/src/main/java/org/jbehave/examples/core/needle/ParentAnnotatedEmbedderUsingNeedle.java
@@ -1,18 +1,68 @@
 package org.jbehave.examples.core.needle;
 
+import static org.needle4j.common.Annotations.assertIsQualifier;
+
+import java.lang.annotation.Annotation;
+
 import org.jbehave.core.InjectableEmbedder;
 import org.jbehave.core.annotations.Configure;
 import org.jbehave.core.annotations.UsingEmbedder;
 import org.jbehave.core.annotations.needle.UsingNeedle;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.junit.needle.NeedleAnnotatedEmbedderRunner;
-import org.jbehave.examples.core.needle.provider.TraderServiceInjectionProvider;
+import org.jbehave.examples.core.needle.ParentAnnotatedEmbedderUsingNeedle.TradingServiceInjectionProvider;
+import org.jbehave.examples.core.service.TradingService;
 import org.junit.runner.RunWith;
+import org.needle4j.injection.InjectionProvider;
+import org.needle4j.injection.InjectionTargetInformation;
 
 @RunWith(NeedleAnnotatedEmbedderRunner.class)
 @Configure()
 @UsingEmbedder(embedder = Embedder.class, generateViewAfterStories = true, ignoreFailureInStories = true, ignoreFailureInView = true)
-@UsingNeedle(provider = TraderServiceInjectionProvider.class)
+@UsingNeedle(provider = TradingServiceInjectionProvider.class)
 public abstract class ParentAnnotatedEmbedderUsingNeedle extends InjectableEmbedder {
+
+  /**
+   * Inline provider
+   * 
+   * @author Simon Zambrovski
+   * 
+   */
+  public static class TradingServiceInjectionProvider implements InjectionProvider<TradingService> {
+
+    protected final TradingService instance = new TradingService();
+
+    public TradingServiceInjectionProvider() {
+    }
+
+    public Object getKey(final InjectionTargetInformation injectionTargetInformation) {
+      return injectionTargetInformation.getType();
+    }
+
+    public boolean verify(final InjectionTargetInformation injectionTargetInformation) {
+      return isTargetAssignable(injectionTargetInformation);
+    }
+
+    public TradingService getInjectedObject(Class<?> injectionPointType) {
+      return instance;
+    }
+
+    /**
+     * <code>true</code> when injection target is or extends/implements instance
+     * type
+     * 
+     * @param injectionTargetInformation
+     * @return true when type is assignable from instance
+     */
+    protected boolean isTargetAssignable(final InjectionTargetInformation injectionTargetInformation) {
+      return injectionTargetInformation.getType().isAssignableFrom(instance.getClass());
+    }
+
+    protected boolean isTargetQualifierPresent(final InjectionTargetInformation injectionTargetInformation, final Class<? extends Annotation> qualifier) {
+      assertIsQualifier(qualifier);
+      return injectionTargetInformation.isAnnotationPresent(qualifier);
+    }
+
+  }
 
 }
