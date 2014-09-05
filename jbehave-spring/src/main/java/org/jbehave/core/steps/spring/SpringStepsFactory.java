b0e1b25c80c9796556c20b9ee6d7a9c8672d39df
Mauro Talevi <mauro.talevi@aquilonia.org>
2014-09-05 22:11:37 +0100

    JBEHAVE-1045:  Tidy up.  Ensured test results should not depend on the order of the steps classes found.

--- jbehave-spring/src/main/java/org/jbehave/core/steps/spring/SpringStepsFactory.java
+++ jbehave-spring/src/main/java/org/jbehave/core/steps/spring/SpringStepsFactory.java
@@ -23,62 +23,63 @@ import org.springframework.util.ClassUtils;
  */
 public class SpringStepsFactory extends AbstractStepsFactory {
 
-    private final ApplicationContext context;
+	private final ApplicationContext context;
 
-    public SpringStepsFactory(Configuration configuration, ApplicationContext context) {
-        super(configuration);
-        this.context = context;
-    }
+	public SpringStepsFactory(Configuration configuration,
+			ApplicationContext context) {
+		super(configuration);
+		this.context = context;
+	}
 
-    @Override
-    protected List<Class<?>> stepsTypes() {
-        // Using set because in some cases (eg: scoped proxies),
-        // there may be two actual beans for each defined bean).
-        Set<Class<?>> types = new HashSet<Class<?>>();
-        for (String name : context.getBeanDefinitionNames()) {
-            Class<?> type = extractType(name);
-            if (isAllowed(type) && hasAnnotatedMethods(type)) {
-                types.add(type);
-            }
-        }
-        return new ArrayList<Class<?>>(types);
-    }
+	@Override
+	protected List<Class<?>> stepsTypes() {
+		// Using set because in some cases (eg: scoped proxies),
+		// there may be two actual beans for each defined bean).
+		Set<Class<?>> types = new HashSet<Class<?>>();
+		for (String name : context.getBeanDefinitionNames()) {
+			Class<?> type = beanType(name);
+			if (isAllowed(type) && hasAnnotatedMethods(type)) {
+				types.add(type);
+			}
+		}
+		return new ArrayList<Class<?>>(types);
+	}
 
-    /**
-     * Checks if type returned from context is allowed, i.e. not null and not
-     * abstract.
-     * 
-     * @param type the Class of the bean
-     * @return A boolean, <code>true</code> if allowed
-     */
-    protected boolean isAllowed(Class<?> type) {
-        return type != null && !Modifier.isAbstract(type.getModifiers());
-    }
+	/**
+	 * Checks if type returned from context is allowed, i.e. not null and not
+	 * abstract.
+	 * 
+	 * @param type
+	 *            the Class of the bean
+	 * @return A boolean, <code>true</code> if allowed
+	 */
+	protected boolean isAllowed(Class<?> type) {
+		return type != null && !Modifier.isAbstract(type.getModifiers());
+	}
 
-    public Object createInstanceOfType(Class<?> type) {
-        for (String name : context.getBeanDefinitionNames()) {
-            Class<?> beanType = extractType(name);
-            if (type.equals(beanType)) {
-                return context.getBean(name);
-            }
-        }
+	public Object createInstanceOfType(Class<?> type) {
+		for (String name : context.getBeanDefinitionNames()) {
+			Class<?> beanType = beanType(name);
+			if (type.equals(beanType)) {
+				return context.getBean(name);
+			}
+		}
+		throw new StepsInstanceNotFound(type, this);
+	}
 
-        throw new StepsInstanceNotFound(type, this);
-    }
-    
-    /**
-     * Handles the type extraction from the bean name, untangling the proxy if
-     * needed
-     * 
-     * @param name
-     * @return
-     */
-    private Class<?> extractType(String name) {
-        Class<?> type = context.getType(name);
-        if (ClassUtils.isCglibProxyClass(type)) {
-            return AopProxyUtils.ultimateTargetClass(context.getBean(name));
-        }
-        return type;
-    }
+	/**
+	 * Return the bean type, untangling the proxy if needed
+	 * 
+	 * @param name
+	 *            the bean name
+	 * @return The Class of the bean
+	 */
+	private Class<?> beanType(String name) {
+		Class<?> type = context.getType(name);
+		if (ClassUtils.isCglibProxyClass(type)) {
+			return AopProxyUtils.ultimateTargetClass(context.getBean(name));
+		}
+		return type;
+	}
 
 }
--- jbehave-spring/src/test/java/org/jbehave/core/configuration/spring/SpringAnnotationBuilderBehaviour.java
+++ jbehave-spring/src/test/java/org/jbehave/core/configuration/spring/SpringAnnotationBuilderBehaviour.java
@@ -46,162 +46,228 @@ import org.springframework.context.ApplicationContext;
 
 public class SpringAnnotationBuilderBehaviour {
 
-    @Test
-    public void shouldBuildConfigurationFromAnnotations() {
-        SpringAnnotationBuilder builder = new SpringAnnotationBuilder(AnnotatedUsingSpring.class);
-        Configuration configuration = builder.buildConfiguration();
-        assertThat(configuration.storyControls().dryRun(), is(true));
-        assertThat(configuration.storyControls().skipScenariosAfterFailure(), is(true));
-        assertThat(configuration.failureStrategy(), instanceOf(SilentlyAbsorbingFailure.class));
-        assertThat(configuration.storyLoader(), instanceOf(LoadFromURL.class));
-        assertThat(configuration.stepPatternParser(), instanceOf(RegexPrefixCapturingPatternParser.class));
-        assertThat(((RegexPrefixCapturingPatternParser) configuration.stepPatternParser()).getPrefix(),
-                equalTo("MyPrefix"));
-        assertThatDateIsConvertedWithFormat(configuration.parameterConverters(), new SimpleDateFormat("yyyy-MM-dd"));
-        assertThat(configuration.parameterControls().nameDelimiterLeft(), equalTo("["));
-        assertThat(configuration.parameterControls().nameDelimiterRight(), equalTo("]"));
-        assertThat(configuration.storyReporterBuilder().formats(), hasItems(CONSOLE, HTML, TXT, XML, STATS));
-        Keywords keywords = configuration.storyReporterBuilder().keywords();
-        assertThat(keywords, instanceOf(LocalizedKeywords.class));
-        assertThat(((LocalizedKeywords) keywords).getLocale(), equalTo(Locale.ITALIAN));
-        assertThat(configuration.storyReporterBuilder().outputDirectory().getName(), equalTo("my-output-directory"));
-        assertThat(configuration.storyReporterBuilder().viewResources().getProperty("index"),
-                equalTo("my-reports-index.ftl"));
-        assertThat(configuration.storyReporterBuilder().viewResources().getProperty("decorateNonHtml"), equalTo("true"));
-        assertThat(configuration.storyReporterBuilder().reportFailureTrace(), is(true));
-    }
+	@Test
+	public void shouldBuildConfigurationFromAnnotations() {
+		SpringAnnotationBuilder builder = new SpringAnnotationBuilder(
+				AnnotatedUsingSpring.class);
+		Configuration configuration = builder.buildConfiguration();
+		assertThat(configuration.storyControls().dryRun(), is(true));
+		assertThat(configuration.storyControls().skipScenariosAfterFailure(),
+				is(true));
+		assertThat(configuration.failureStrategy(),
+				instanceOf(SilentlyAbsorbingFailure.class));
+		assertThat(configuration.storyLoader(), instanceOf(LoadFromURL.class));
+		assertThat(configuration.stepPatternParser(),
+				instanceOf(RegexPrefixCapturingPatternParser.class));
+		assertThat(
+				((RegexPrefixCapturingPatternParser) configuration.stepPatternParser())
+						.getPrefix(), equalTo("MyPrefix"));
+		assertThatDateIsConvertedWithFormat(
+				configuration.parameterConverters(), new SimpleDateFormat(
+						"yyyy-MM-dd"));
+		assertThat(configuration.parameterControls().nameDelimiterLeft(),
+				equalTo("["));
+		assertThat(configuration.parameterControls().nameDelimiterRight(),
+				equalTo("]"));
+		assertThat(configuration.storyReporterBuilder().formats(),
+				hasItems(CONSOLE, HTML, TXT, XML, STATS));
+		Keywords keywords = configuration.storyReporterBuilder().keywords();
+		assertThat(keywords, instanceOf(LocalizedKeywords.class));
+		assertThat(((LocalizedKeywords) keywords).getLocale(),
+				equalTo(Locale.ITALIAN));
+		assertThat(configuration.storyReporterBuilder().outputDirectory()
+				.getName(), equalTo("my-output-directory"));
+		assertThat(configuration.storyReporterBuilder().viewResources()
+				.getProperty("index"), equalTo("my-reports-index.ftl"));
+		assertThat(configuration.storyReporterBuilder().viewResources()
+				.getProperty("decorateNonHtml"), equalTo("true"));
+		assertThat(configuration.storyReporterBuilder().reportFailureTrace(),
+				is(true));
+	}
 
-    private void assertThatDateIsConvertedWithFormat(ParameterConverters parameterConverters, DateFormat dateFormat) {
-        String date = "2010-10-10";
-        try {
-            assertThat((Date) parameterConverters.convert(date, Date.class), equalTo(dateFormat.parse(date)));
-        } catch (ParseException e) {
-            fail();
-        }
-    }
+	private void assertThatDateIsConvertedWithFormat(
+			ParameterConverters parameterConverters, DateFormat dateFormat) {
+		String date = "2010-10-10";
+		try {
+			assertThat((Date) parameterConverters.convert(date, Date.class),
+					equalTo(dateFormat.parse(date)));
+		} catch (ParseException e) {
+			fail();
+		}
+	}
 
-    @Test
-    public void shouldBuildDefaultConfigurationIfAnnotationOrAnnotatedValuesNotPresent() {
-        SpringAnnotationBuilder builderNotAnnotated = new SpringAnnotationBuilder(NotAnnotated.class);
-        assertThatConfigurationIs(builderNotAnnotated.buildConfiguration(), new MostUsefulConfiguration());
-        SpringAnnotationBuilder builderAnnotatedWithoutLocations = new SpringAnnotationBuilder(
-                AnnotatedWithoutResources.class);
-        assertThatConfigurationIs(builderAnnotatedWithoutLocations.buildConfiguration(), new MostUsefulConfiguration());
-    }
+	@Test
+	public void shouldBuildDefaultConfigurationIfAnnotationOrAnnotatedValuesNotPresent() {
+		SpringAnnotationBuilder builderNotAnnotated = new SpringAnnotationBuilder(
+				NotAnnotated.class);
+		assertThatConfigurationIs(builderNotAnnotated.buildConfiguration(),
+				new MostUsefulConfiguration());
+		SpringAnnotationBuilder builderAnnotatedWithoutLocations = new SpringAnnotationBuilder(
+				AnnotatedWithoutResources.class);
+		assertThatConfigurationIs(
+				builderAnnotatedWithoutLocations.buildConfiguration(),
+				new MostUsefulConfiguration());
+	}
 
-    private void assertThatConfigurationIs(Configuration builtConfiguration, Configuration defaultConfiguration) {
-        assertThat(builtConfiguration.failureStrategy(), instanceOf(defaultConfiguration.failureStrategy().getClass()));
-        assertThat(builtConfiguration.storyLoader(), instanceOf(defaultConfiguration.storyLoader().getClass()));
-        assertThat(builtConfiguration.stepPatternParser(), instanceOf(defaultConfiguration.stepPatternParser()
-                .getClass()));
-        assertThat(builtConfiguration.storyReporterBuilder().formats(), equalTo(defaultConfiguration
-                .storyReporterBuilder().formats()));
-        assertThat(builtConfiguration.storyReporterBuilder().outputDirectory(), equalTo(defaultConfiguration
-                .storyReporterBuilder().outputDirectory()));
-        assertThat(builtConfiguration.storyReporterBuilder().viewResources(), equalTo(defaultConfiguration
-                .storyReporterBuilder().viewResources()));
-        assertThat(builtConfiguration.storyReporterBuilder().reportFailureTrace(), equalTo(defaultConfiguration
-                .storyReporterBuilder().reportFailureTrace()));
-    }
+	private void assertThatConfigurationIs(Configuration builtConfiguration,
+			Configuration defaultConfiguration) {
+		assertThat(builtConfiguration.failureStrategy(),
+				instanceOf(defaultConfiguration.failureStrategy().getClass()));
+		assertThat(builtConfiguration.storyLoader(),
+				instanceOf(defaultConfiguration.storyLoader().getClass()));
+		assertThat(builtConfiguration.stepPatternParser(),
+				instanceOf(defaultConfiguration.stepPatternParser().getClass()));
+		assertThat(builtConfiguration.storyReporterBuilder().formats(),
+				equalTo(defaultConfiguration.storyReporterBuilder().formats()));
+		assertThat(builtConfiguration.storyReporterBuilder().outputDirectory(),
+				equalTo(defaultConfiguration.storyReporterBuilder()
+						.outputDirectory()));
+		assertThat(builtConfiguration.storyReporterBuilder().viewResources(),
+				equalTo(defaultConfiguration.storyReporterBuilder()
+						.viewResources()));
+		assertThat(builtConfiguration.storyReporterBuilder()
+				.reportFailureTrace(), equalTo(defaultConfiguration
+				.storyReporterBuilder().reportFailureTrace()));
+	}
 
-    @Test
-    public void shouldBuildCandidateStepsFromAnnotationsUsingSpring() {
-        SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(AnnotatedUsingSpring.class);
-        Configuration configuration = builderAnnotated.buildConfiguration();
-        assertThatStepsInstancesAre(builderAnnotated.buildCandidateSteps(configuration), FooSteps.class,
-                FooStepsWithDependency.class);
-    }
+	@Test
+	public void shouldBuildCandidateStepsFromAnnotationsUsingSpring() {
+		SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(
+				AnnotatedUsingSpring.class);
+		Configuration configuration = builderAnnotated.buildConfiguration();
+		assertThatStepsInstancesAreFound(
+				builderAnnotated.buildCandidateSteps(configuration),
+				FooSteps.class, FooStepsWithDependency.class);
+	}
 
-    @Test
-    public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndSpring() {
-        SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(AnnotatedUsingStepsAndSpring.class);
-        Configuration configuration = builderAnnotated.buildConfiguration();
-        assertThatStepsInstancesAre(builderAnnotated.buildCandidateSteps(configuration), FooSteps.class);
-    }
+	@Test
+	public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndSpring() {
+		SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(
+				AnnotatedUsingStepsAndSpring.class);
+		Configuration configuration = builderAnnotated.buildConfiguration();
+		assertThatStepsInstancesAre(
+				builderAnnotated.buildCandidateSteps(configuration),
+				FooSteps.class);
+	}
 
-    @Test
-    public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndInheritingPicoFromParent() {
-        AnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(InheritingAnnotatedUsingSteps.class);
-        Configuration configuration = builderAnnotated.buildConfiguration();
-        assertThatStepsInstancesAre(builderAnnotated.buildCandidateSteps(configuration), FooSteps.class);
-    }
-    
-    @Test
-    public void shouldBuildEmptyStepsListIfAnnotationOrAnnotatedValuesNotPresent() {
-        SpringAnnotationBuilder builderNotAnnotated = new SpringAnnotationBuilder(NotAnnotated.class);
-        assertThatStepsInstancesAre(builderNotAnnotated.buildCandidateSteps());
-        SpringAnnotationBuilder builderAnnotatedWithoutResources = new SpringAnnotationBuilder(
-                AnnotatedWithoutResources.class);
-        assertThatStepsInstancesAre(builderAnnotatedWithoutResources.buildCandidateSteps());
-    }
+	@Test
+	public void shouldBuildCandidateStepsFromAnnotationsUsingStepsAndInheritingPicoFromParent() {
+		AnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(
+				InheritingAnnotatedUsingSteps.class);
+		Configuration configuration = builderAnnotated.buildConfiguration();
+		assertThatStepsInstancesAre(
+				builderAnnotated.buildCandidateSteps(configuration),
+				FooSteps.class);
+	}
 
-    private void assertThatStepsInstancesAre(List<CandidateSteps> candidateSteps, Class<?>... stepsClasses) {
-        assertThat(candidateSteps.size(), equalTo(stepsClasses.length));
-        for (int i = 0; i < stepsClasses.length; i++) {
-            assertThat(((Steps) candidateSteps.get(i)).instance(), instanceOf(stepsClasses[i]));
-        }
-    }
+	@Test
+	public void shouldBuildEmptyStepsListIfAnnotationOrAnnotatedValuesNotPresent() {
+		SpringAnnotationBuilder builderNotAnnotated = new SpringAnnotationBuilder(
+				NotAnnotated.class);
+		assertThatStepsInstancesAre(builderNotAnnotated.buildCandidateSteps());
+		SpringAnnotationBuilder builderAnnotatedWithoutResources = new SpringAnnotationBuilder(
+				AnnotatedWithoutResources.class);
+		assertThatStepsInstancesAre(builderAnnotatedWithoutResources
+				.buildCandidateSteps());
+	}
 
-    @Test
-    public void shouldNotBuildContainerIfResourceNotLoadable() {
-        AnnotationMonitor annotationMonitor = mock(AnnotationMonitor.class);
-        AnnotationBuilder builderUnloadableResource = new SpringAnnotationBuilder(AnnotatedWithUnloadableResource.class, annotationMonitor);
-        try {
-            assertThatStepsInstancesAre(builderUnloadableResource.buildCandidateSteps());
-            fail("Exception expected");
-        } catch (AnnotationBuilder.InstantiationFailed e) {
-            // expected
-        }
-        verify(annotationMonitor).elementCreationFailed(isA(Class.class), isA(Exception.class));
-    }
+	private void assertThatStepsInstancesAre(
+			List<CandidateSteps> candidateSteps, Class<?>... stepsClasses) {
+		assertThat(candidateSteps.size(), equalTo(stepsClasses.length));
+		for (int i = 0; i < stepsClasses.length; i++) {
+			assertThat(((Steps) candidateSteps.get(i)).instance(),
+					instanceOf(stepsClasses[i]));
+		}
+	}
 
-    @Test
-    public void shouldCreateOnlyOneContainerForMultipleBuildInvocations() {
-        SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(AnnotatedUsingStepsAndSpring.class);
-        builderAnnotated.buildConfiguration();
-        ApplicationContext applicationContext = builderAnnotated.applicationContext();
-        builderAnnotated.buildConfiguration();
-        assertThat(builderAnnotated.applicationContext(), sameInstance(applicationContext));
-    }
+	private void assertThatStepsInstancesAreFound(
+			List<CandidateSteps> candidateSteps, Class<?>... stepsClasses) {
+		boolean found = false;
+		for (Class<?> stepsClass : stepsClasses) {
+			for (CandidateSteps candidates : candidateSteps) {
+				if (((Steps) candidates).instance().getClass()
+						.equals(stepsClass)) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				Assert.fail("Class " + stepsClass + " not found in "
+						+ candidateSteps);
+			}
+		}
+	}
 
-    @Configure()
-    @UsingSpring(resources = { "org/jbehave/core/configuration/spring/configuration.xml",
-            "org/jbehave/core/steps/spring/steps.xml", "org/jbehave/core/steps/spring/steps-with-dependency.xml" })
-    private static class AnnotatedUsingSpring {
+	@Test
+	public void shouldNotBuildContainerIfResourceNotLoadable() {
+		AnnotationMonitor annotationMonitor = mock(AnnotationMonitor.class);
+		AnnotationBuilder builderUnloadableResource = new SpringAnnotationBuilder(
+				AnnotatedWithUnloadableResource.class, annotationMonitor);
+		try {
+			assertThatStepsInstancesAre(builderUnloadableResource
+					.buildCandidateSteps());
+			fail("Exception expected");
+		} catch (AnnotationBuilder.InstantiationFailed e) {
+			// expected
+		}
+		verify(annotationMonitor).elementCreationFailed(isA(Class.class),
+				isA(Exception.class));
+	}
 
-    }
+	@Test
+	public void shouldCreateOnlyOneContainerForMultipleBuildInvocations() {
+		SpringAnnotationBuilder builderAnnotated = new SpringAnnotationBuilder(
+				AnnotatedUsingStepsAndSpring.class);
+		builderAnnotated.buildConfiguration();
+		ApplicationContext applicationContext = builderAnnotated
+				.applicationContext();
+		builderAnnotated.buildConfiguration();
+		assertThat(builderAnnotated.applicationContext(),
+				sameInstance(applicationContext));
+	}
 
-    @Configure()
-    @UsingSteps(instances = { FooSteps.class })
-    @UsingSpring(resources = { "org/jbehave/core/configuration/spring/configuration.xml" })
-    private static class AnnotatedUsingStepsAndSpring {
+	@Configure()
+	@UsingSpring(resources = {
+			"org/jbehave/core/configuration/spring/configuration.xml",
+			"org/jbehave/core/steps/spring/steps.xml",
+			"org/jbehave/core/steps/spring/steps-with-dependency.xml" })
+	private static class AnnotatedUsingSpring {
 
-    }
+	}
 
-    @Configure()
-    @UsingSpring(resources = { "org/jbehave/core/configuration/spring/configuration.xml" })
-    private static class ParentAnnotatedUsingSpring {
+	@Configure()
+	@UsingSteps(instances = { FooSteps.class })
+	@UsingSpring(resources = { "org/jbehave/core/configuration/spring/configuration.xml" })
+	private static class AnnotatedUsingStepsAndSpring {
 
-    }
-    
-    @UsingSteps(instances = { FooSteps.class })
-    private static class InheritingAnnotatedUsingSteps extends ParentAnnotatedUsingSpring {
+	}
 
-    }
-    @Configure()
-    @UsingSpring()
-    private static class AnnotatedWithoutResources {
+	@Configure()
+	@UsingSpring(resources = { "org/jbehave/core/configuration/spring/configuration.xml" })
+	private static class ParentAnnotatedUsingSpring {
 
-    }
+	}
 
-    @Configure()
-    @UsingSpring(resources = { "inexistent"}, ignoreContextFailure = false )
-    private static class AnnotatedWithUnloadableResource {
+	@UsingSteps(instances = { FooSteps.class })
+	private static class InheritingAnnotatedUsingSteps extends
+			ParentAnnotatedUsingSpring {
 
-    }
-    
-    private static class NotAnnotated {
+	}
 
-    }
+	@Configure()
+	@UsingSpring()
+	private static class AnnotatedWithoutResources {
+
+	}
+
+	@Configure()
+	@UsingSpring(resources = { "inexistent" }, ignoreContextFailure = false)
+	private static class AnnotatedWithUnloadableResource {
+
+	}
+
+	private static class NotAnnotated {
+
+	}
 
 }
