0de5aa2ec14cf17b225b39e4a575a677c100cb44
Mauro Talevi <mauro.talevi@aquilonia.org>
2014-10-05 00:06:56 +0200

    JBEHAVE-1046:  Added stories configuration to verify failing behaviour upon pending steps.

---
+++ examples/core/src/main/java/org/jbehave/examples/core/CoreStoriesFailingUponPending.java
@@ -0,0 +1,26 @@
+package org.jbehave.examples.core;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+
+import java.util.List;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.failures.FailingUponPendingStep;
+import org.jbehave.core.io.StoryFinder;
+
+/**
+ */
+public class CoreStoriesFailingUponPending extends CoreStories {
+
+	public Configuration configuration() {
+		return super.configuration()       		
+					.usePendingStepStrategy(new FailingUponPendingStep());
+	}
+
+    @Override
+    protected List<String> storyPaths() {
+        String filter = System.getProperty("story.filter", "**/pending.story");
+        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), filter, "");
+    }
+
+}
