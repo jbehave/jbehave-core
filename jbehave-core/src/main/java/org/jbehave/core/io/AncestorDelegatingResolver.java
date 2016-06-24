027f600b3d35c8608209b381a64599c44d77300b
Mauro Talevi <mauro.talevi@aquilonia.org>
2016-06-24 13:21:15 +0100

    JBEHAVE-1178: Added AncestorDelegatingResolver.

---
+++ jbehave-core/src/main/java/org/jbehave/core/io/AncestorDelegatingResolver.java
@@ -0,0 +1,40 @@
+package org.jbehave.core.io;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class AncestorDelegatingResolver implements StoryNameResolver {
+
+	private static final String SEPARATOR = "/";
+	private static final String SPACE = " ";
+	
+    private final int ancestors;
+    private final StoryNameResolver delegate; 
+
+    public AncestorDelegatingResolver() {
+    	this(1);
+	}
+
+    public AncestorDelegatingResolver(int ancestors) {
+    	this(ancestors, new UnderscoredToCapitalized());
+	}
+
+    public AncestorDelegatingResolver(int ancestors, StoryNameResolver delegate) {
+		this.ancestors = ancestors;
+		this.delegate = delegate;
+	}
+
+	public String resolveName(String path) {
+		List<String> reversed = Arrays.asList(path.split(SEPARATOR));
+		Collections.reverse(reversed);
+		List<String> names = new ArrayList<String>();
+		for (int i = 0; i < ancestors + 1; i++) {
+			names.add(0, delegate.resolveName(reversed.get(i)));
+		}
+		return StringUtils.join(names,SPACE);
+	}
+}
--- jbehave-core/src/test/java/org/jbehave/core/io/StoryNameResolverBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/io/StoryNameResolverBehaviour.java
@@ -1,7 +1,9 @@
 package org.jbehave.core.io;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -27,6 +29,27 @@ public class StoryNameResolverBehaviour {
         assertThat(resolver.resolveName("camel_case.ext"), equalTo("Camel Case"));
         assertThat(resolver.resolveName("org.jbehave.core.io.camel_case"), equalTo("Camel Case"));
         assertThat(resolver.resolveName("camel_case"), equalTo("Camel Case"));
+    }
+
+    @Test
+    public void shouldResolveAncestorWithDefaultAncestors() {
+        StoryNameResolver resolver = new AncestorDelegatingResolver();
+        assertThat(resolver.resolveName("org/jbehave/core/io/camel_case.story"), equalTo("Io Camel Case"));
+    }
+
+    @Test
+    public void shouldResolveAncestorWithCustomAncestors() {
+        StoryNameResolver resolver = new AncestorDelegatingResolver(2);
+        assertThat(resolver.resolveName("org/jbehave/core/io/camel_case.story"), equalTo("Core Io Camel Case"));
+    }
+
+    @Test
+    public void shouldResolveAncestorWithCustomDelegate() {
+        StoryNameResolver delegate = mock(StoryNameResolver.class);
+		when(delegate.resolveName("io")).thenReturn("IO");
+		when(delegate.resolveName("camel_case.story")).thenReturn("CC");
+		StoryNameResolver resolver = new AncestorDelegatingResolver(1, delegate);
+        assertThat(resolver.resolveName("org/jbehave/core/io/camel_case.story"), equalTo("IO CC"));
     }
 
 }
