3cb91db19c70e746702c14af793d0b96c04c011b
Mauro Talevi <mauro.talevi@aquilonia.org>
2017-09-08 18:38:45 +0200

    Revert "JBEHAVE-1138 GivenStories at story level are reported to StoryReporter chaotically in multi-threaded mode"
    This reverts commit 04345e25c0917d57f6e8cbacdfa5df060636d47d.

--- jbehave-core/src/main/java/org/jbehave/core/embedder/PerformableTree.java
+++ jbehave-core/src/main/java/org/jbehave/core/embedder/PerformableTree.java
@@ -787,7 +787,7 @@ public class PerformableTree {
                 this.status = Status.NOT_ALLOWED;
             }
             context.stepsContext().resetStory();
-            context.reporter().beforeStory(story, givenStory);
+            context.reporter().beforeStory(story, context.givenStory);
             context.reporter().narrative(story.getNarrative());
             context.reporter().lifecyle(story.getLifecycle());
             State state = context.state();
@@ -803,7 +803,7 @@ public class PerformableTree {
             if (context.restartStory()) {
                 context.reporter().afterStory(true);
             } else {
-                context.reporter().afterStory(givenStory);
+                context.reporter().afterStory(context.givenStory);
             }
             this.status = context.status(state);
         }
@@ -811,9 +811,12 @@ public class PerformableTree {
         private void performGivenStories(RunContext context) throws InterruptedException {
             if (givenStories.size() > 0) {
                 context.reporter().givenStories(story.getGivenStories());
+                final boolean parentGivenStory = context.givenStory;
                 for (PerformableStory story : givenStories) {
+                    context.givenStory = story.givenStory();
                     story.perform(context);
                 }
+                context.givenStory = parentGivenStory;
            }
         }
 
@@ -981,9 +984,12 @@ public class PerformableTree {
             beforeSteps.perform(context);
 			if (givenStories.size() > 0) {
 				context.reporter().givenStories(scenario.getGivenStories());
+                final boolean parentGivenStory = context.givenStory;
 				for (PerformableStory story : givenStories) {
+					context.givenStory = story.givenStory();
 					story.perform(context);
 				}
+				context.givenStory = parentGivenStory;
 			}
 			performRestartableSteps(context);	        
             afterSteps.perform(context);
@@ -1013,9 +1019,12 @@ public class PerformableTree {
             beforeSteps.perform(context);
 			if (givenStories.size() > 0) {
 				context.reporter().givenStories(scenario.getGivenStories());
+				final boolean parentGivenStory = context.givenStory;
 				for (PerformableStory story : givenStories) {
+					context.givenStory = story.givenStory();
 					story.perform(context);
 				}
+				context.givenStory = parentGivenStory;
 			}
 			performRestartableSteps(context);	        
             afterSteps.perform(context);
