af740d589d82d4d5ca7887df5bfb0ece943d3e52
Valery Yatsynovich <valfirst@yandex.ru>
2019-04-29 23:45:16 +0300

    JBEHAVE-1342 Set minimal required JDK to 1.8

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Keep an eye on the  [Travis CI](http://travis-ci.org/jbehave) server for JBehave
 
 JDK version required: 
 
-1.7 or above at build and runtime (tested with Oracle JDK on different platforms)
+1.8 or above at build and runtime (tested with Oracle JDK on different platforms)
 
 ### Maven 
 
--- examples/gherkin/src/main/java/org/jbehave/examples/gherkin/steps/GherkinSteps.java
+++ examples/gherkin/src/main/java/org/jbehave/examples/gherkin/steps/GherkinSteps.java
@@ -9,34 +9,35 @@ import org.jbehave.core.steps.Parameters;
 
 public class GherkinSteps {
     
-	private Car car;
+    private Car car;
 
     @Given("I have a license")
     public void givenIHaveALicense() {
     }
-	
-	@Given("I have a car")
-	public void givenIHaveACar() {
-		car = new Car();
-	}
 
-	@Then("I can drive them according to wheels: $table")
-	public void thenICanDriveThemAccordingTo(ExamplesTable table) {
-		OutcomesTable outcomes = new OutcomesTable();
-		for ( Parameters row : table.getRowsAsParameters() ){
-			Integer wheels = row.valueAs("wheels", Integer.class);
-			Boolean canDriveWith = car.canDriveWith(wheels);
-			outcomes.addOutcome("wheels "+wheels, canDriveWith, Matchers.is(row.valueAs("can_drive", Boolean.class)));
-		}
-		outcomes.verify();
-	}
-	
-	private static class Car {
+    @Given("I have a car")
+    public void givenIHaveACar() {
+        car = new Car();
+    }
 
-		public boolean canDriveWith(int wheels) {
+    @Then("I can drive them according to wheels: $table")
+    public void thenICanDriveThemAccordingTo(ExamplesTable table) {
+        OutcomesTable outcomes = new OutcomesTable();
+        for ( Parameters row : table.getRowsAsParameters() ){
+            Integer wheels = row.valueAs("wheels", Integer.class);
+            Boolean canDriveWith = car.canDriveWith(wheels);
+            Boolean canDrive = row.valueAs("can_drive", Boolean.class);
+            outcomes.addOutcome("wheels " + wheels, canDriveWith, Matchers.is(canDrive));
+        }
+        outcomes.verify();
+    }
+
+    private static class Car {
+
+        public boolean canDriveWith(int wheels) {
             return wheels == 4;
         }
-		
-	}
+
+    }
 
 }
