68b531c9d5146206261e34982579ae3ca25e9da7
Paul Hammant <paul@hammant.org>
2021-01-18 08:24:28 +0000

    take out unused getSteps

--- jbehave-core/src/main/java/org/jbehave/core/context/Context.java
+++ jbehave-core/src/main/java/org/jbehave/core/context/Context.java
@@ -3,8 +3,6 @@ package org.jbehave.core.context;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import java.util.List;
-
 /**
  * Holds context-related information
  */
@@ -12,7 +10,6 @@ public class Context {
 	
 	private ThreadLocal<String> currentStory = new ThreadLocal<>();
 	private ThreadLocal<String> currentScenario = new ThreadLocal<>();
-	private ThreadLocal<List<String>> currentSteps = new ThreadLocal<>();
 
     public String getCurrentStory() {
 		return currentStory.get();
@@ -30,20 +27,11 @@ public class Context {
 		this.currentScenario.set(currentScenario);
 	}
 
-	public List<String> getCurrentSteps() {
-		return currentSteps.get();
-	}
-
-	public void setCurrentSteps(List<String> currentSteps) {
-		this.currentSteps.set(currentSteps);
-	}
-
 	@Override
 	public String toString() {
 		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
 				.append("story="+getCurrentStory())
 				.append("scenario="+getCurrentScenario())
-				.append("steps="+getCurrentSteps())
 				.build();
 	}
 
