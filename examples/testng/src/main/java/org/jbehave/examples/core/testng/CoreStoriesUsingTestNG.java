88a7d1d511a6f6a6aa81ea306c1b450509a614c9
Mauro Talevi <mauro.talevi@aquilonia.org>
2021-01-27 17:11:48 +0000

    JBEHAVE-1463 Updated testng example.

--- examples/testng/src/main/java/org/jbehave/examples/core/testng/CoreStoriesUsingTestNG.java
+++ examples/testng/src/main/java/org/jbehave/examples/core/testng/CoreStoriesUsingTestNG.java
@@ -1,11 +1,10 @@
 package org.jbehave.examples.core.testng;
 
-import java.util.List;
-
 import org.jbehave.core.Embeddable;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.examples.core.CoreStories;
-import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
 
@@ -16,13 +15,13 @@ import static org.jbehave.core.io.CodeLocations.codeLocationFromPath;
  * <p>
  * It uses the same configuration as the CoreStories, except that the
  * {@link Embeddable#run()} method is annotated by the TestNG
- * {@link Test} annotation.
+ * {@link org.testng.annotations.Test} annotation.
  * </p>
  */
 public class CoreStoriesUsingTestNG extends CoreStories {
 
     @Override
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void run() {
         super.run();
     }
