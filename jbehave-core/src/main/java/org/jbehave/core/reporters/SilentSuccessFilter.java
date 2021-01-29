51ad38d9b5e5ab3b03af064dfa280cdc18bc3dea
Uladzislau Arlouski <uarlouski@gmail.com>
2021-01-29 12:28:38 +0300

    JBEHAVE-1467 Eliminate copy-paste in story reporters

--- jbehave-core/src/main/java/org/jbehave/core/reporters/SilentSuccessFilter.java
+++ jbehave-core/src/main/java/org/jbehave/core/reporters/SilentSuccessFilter.java
@@ -26,68 +26,40 @@ public class SilentSuccessFilter extends NullStoryReporter {
 
     @Override
     public void dryRun() {
-        runState = new State(){
-            @Override
-            public void report(){
-                delegate.dryRun();
-            }
-        };
+        runState = createState(delegate::dryRun);
         runState.report();
     }
     
     @Override
     public void pendingMethods(final List<String> methods) {
-        runState = new State(){
-            @Override
-            public void report(){
-                delegate.pendingMethods(methods);
-            }
-        };
+        runState = createState(() -> delegate.pendingMethods(methods));
         runState.report();
     }
 
     @Override
     public void beforeStory(final Story story, final boolean givenStory) {
         this.givenStory = givenStory;
-        beforeStoryState = new State() {
-            @Override
-            public void report() {
-                delegate.beforeStory(story, givenStory);
-                beforeStoryState = State.SILENT;
-            }
-        };
+        beforeStoryState = createState(() -> {
+            delegate.beforeStory(story, givenStory);
+            beforeStoryState = State.SILENT;
+        });
     }
 
     @Override
     public void narrative(final Narrative narrative) {
-        beforeStoryState = new State() {
-            @Override
-            public void report() {
-                delegate.narrative(narrative);
-            }
-        };
+        beforeStoryState = createState(() -> delegate.narrative(narrative));
         beforeStoryState.report();
     }
 
     @Override
     public void lifecyle(final Lifecycle lifecycle) {
-        beforeStoryState = new State() {
-            @Override
-            public void report() {
-                delegate.lifecyle(lifecycle);
-            }
-        };
+        beforeStoryState = createState(() -> delegate.lifecyle(lifecycle));
         beforeStoryState.report();
     }
 
     @Override
     public void storyNotAllowed(final Story story, final String filter) {
-        beforeStoryState = new State() {
-            @Override
-            public void report() {
-                delegate.storyNotAllowed(story, filter);
-            }
-        };
+        beforeStoryState = createState(() -> delegate.storyNotAllowed(story, filter));
         beforeStoryState.report();
     }
 
@@ -98,161 +70,104 @@ public class SilentSuccessFilter extends NullStoryReporter {
     
     @Override
     public void ignorable(final String step) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.ignorable(step);
-            }
-        });
+        addScenarioTodo(() -> delegate.ignorable(step));
     }
 
     @Override
     public void comment(final String step) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.comment(step);
-            }
-        });
+        addScenarioTodo(() -> delegate.comment(step));
     }
 
     @Override
     public void failed(final String step, final Throwable cause) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.failed(step, cause);
-            }
-        });
+        addScenarioTodo(() -> delegate.failed(step, cause));
         setStateToNoisy();
     }
 
     @Override
     public void failedOutcomes(final String step, final OutcomesTable table) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.failedOutcomes(step, table);
-            }
-        });
+        addScenarioTodo(() -> delegate.failedOutcomes(step, table));
         setStateToNoisy();
     }
 
     @Override
     public void notPerformed(final String step) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.notPerformed(step);
-            }
-        });
+        addScenarioTodo(() -> delegate.notPerformed(step));
         setStateToNoisy();
     }
 
     @Override
     public void pending(final String step) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.pending(step);
-            }
-        });
+        addScenarioTodo(() -> delegate.pending(step));
         setStateToNoisy();
     }
 
     @Override
     public void successful(final String step) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.successful(step);
-            }
-        });
+        addScenarioTodo(() -> delegate.successful(step));
     }
 
     @Override
     public void afterScenario() {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.afterScenario();
-            }
-        });
+        addScenarioTodo(delegate::afterScenario);
         scenarioState.report();
     }
 
     @Override
     public void beforeScenario(final Scenario scenario) {
         scenarioTodos = new ArrayList<>();
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.beforeScenario(scenario);
-            }
-        });
+        addScenarioTodo(() -> delegate.beforeScenario(scenario));
     }
 
     @Override
     public void scenarioNotAllowed(final Scenario scenario, final String filter) {
-        scenarioState = new State() {
-            @Override
-            public void report() {
-                delegate.scenarioNotAllowed(scenario, filter);
-            }
-        };
+        scenarioState = createState(() -> delegate.scenarioNotAllowed(scenario, filter));
         scenarioState.report();
     }
 
     @Override
     public void givenStories(final GivenStories givenStories) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.givenStories(givenStories);
-            }
-        });
+        addScenarioTodo(() -> delegate.givenStories(givenStories));
     }
 
     @Override
     public void givenStories(final List<String> storyPaths) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.givenStories(storyPaths);
-            }
-        });
+        addScenarioTodo(() -> delegate.givenStories(storyPaths));
     }
 
     @Override
     public void beforeExamples(final List<String> steps, final ExamplesTable table) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.beforeExamples(steps, table);
-            }
-        });
+        addScenarioTodo(() -> delegate.beforeExamples(steps, table));
     }
 
     @Override
     public void example(final Map<String, String> tableRow, final int exampleIndex) {
-        scenarioTodos.add(new Todo() {
-            @Override
-            public void doNow() {
-                delegate.example(tableRow, exampleIndex);
-            }
-        });
+        addScenarioTodo(() -> delegate.example(tableRow, exampleIndex));
     }
 
     @Override
     public void afterExamples() {
+        addScenarioTodo(() -> delegate.afterExamples());
+    }
+
+    private void addScenarioTodo(Runnable todoAction) {
         scenarioTodos.add(new Todo() {
             @Override
             public void doNow() {
-                delegate.afterExamples();
+                todoAction.run();
             }
         });
     }
-    
+
+    private State createState(Runnable stateReporter) {
+        return new State() {
+            @Override
+            public void report() {
+                stateReporter.run();;
+            }
+        };
+    }
+
     private static interface Todo {
         void doNow();
     }
