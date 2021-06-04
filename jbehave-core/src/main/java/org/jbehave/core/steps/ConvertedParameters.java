1316fc5c3095199273cf9fa63e143c9a958634d9
Uladzislau Arlouski <uladzislau_arlouski@epam.com>
2021-06-04 13:12:40 +0300

    JBEHAVE-1491 Add type conversion API into Parameters class

--- jbehave-core/src/main/java/org/jbehave/core/steps/ConvertedParameters.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/ConvertedParameters.java
@@ -1,7 +1,18 @@
 package org.jbehave.core.steps;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.jbehave.core.annotations.Parameter;
 
 /**
  * Implementation of Parameters that uses {@link ParameterConverters} to convert
@@ -47,6 +58,79 @@ public class ConvertedParameters implements Parameters {
         return defaultValue;
     }
 
+    @Override
+    public <T> T mapTo(Class<T> type) {
+        return mapTo(type, Collections.emptyMap());
+    }
+
+    @Override
+    public <T> T mapTo(Class<T> type, Map<String, String> fieldNameMapping) {
+        try {
+            T instance = type.getDeclaredConstructor().newInstance();
+
+            for (Entry<String, Field> mappedField : findFields(type, values().keySet(), fieldNameMapping).entrySet()) {
+                Field field = mappedField.getValue();
+                Object value = valueAs(mappedField.getKey(), field.getGenericType());
+                field.setAccessible(true);
+                field.set(instance, value);
+            }
+
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new ParametersNotMappableToType(e);
+        }
+    }
+
+    private static Map<String, Field> findFields(Class<?> type, Set<String> fieldNames,
+            Map<String, String> fieldNameMapping) {
+
+        Map<String, Field> mappedFields = new HashMap<>();
+        List<String> unmappableFields = new ArrayList<>();
+
+        for (String fieldName : fieldNames) {
+            Optional<Field> fieldWrapper = findField(type, fieldName, fieldNameMapping);
+            if (fieldWrapper.isPresent()) {
+                mappedFields.put(fieldName, fieldWrapper.get());
+            } else {
+                unmappableFields.add(fieldName);
+            }
+        }
+
+        if (unmappableFields.isEmpty()) {
+            return mappedFields;
+        }
+
+        throw new ParametersNotMappableToType(String.format("Unable to map %s field(s) for type %s", unmappableFields,
+                type));
+    }
+
+    private static <T> Optional<Field> findField(Class<T> type, String name, Map<String, String> fieldNameMapping) {
+
+        String mapping = fieldNameMapping.get(name);
+        String fieldName = mapping == null ? name : mapping;
+
+        Optional<Field> field = Stream.of(type.getDeclaredFields())
+                                      .filter(f -> f.isAnnotationPresent(Parameter.class))
+                                      .filter(f -> fieldName.equals(f.getAnnotation(Parameter.class).name()))
+                                      .findFirst();
+
+        return field.isPresent() ? field : findField(type, fieldName);
+    }
+
+    private static Optional<Field> findField(Class<?> type, String fieldName) {
+        for (Field field : type.getDeclaredFields()) {
+            if (field.getName().equals(fieldName)) {
+                return Optional.of(field);
+            }
+        }
+
+        if (type.getSuperclass() != null) {
+            return findField(type.getSuperclass(), fieldName);
+        }
+
+        return Optional.empty();
+    }
+
     @SuppressWarnings("unchecked")
     private <T> T convert(String value, Type type) {
         return (T) parameterConverters.convert(value, type);
@@ -73,4 +157,16 @@ public class ConvertedParameters implements Parameters {
 
     }
 
+    @SuppressWarnings("serial")
+    public static class ParametersNotMappableToType extends RuntimeException {
+
+        public ParametersNotMappableToType(String message) {
+            super(message);
+        }
+
+        public ParametersNotMappableToType(Exception cause) {
+            super(cause);
+        }
+
+    }
 }
--- jbehave-core/src/test/java/org/jbehave/core/steps/ConvertedParametersBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/steps/ConvertedParametersBehaviour.java
@@ -2,20 +2,26 @@ package org.jbehave.core.steps;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jbehave.core.annotations.Parameter;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.model.TableTransformers;
+import org.jbehave.core.steps.ConvertedParameters.ParametersNotMappableToType;
 import org.junit.jupiter.api.Test;
 
 class ConvertedParametersBehaviour {
 
-    private Map<String, String> map = Collections.singletonMap("one", "11");
-    private Parameters parameters = new ConvertedParameters(map,
-            new ParameterConverters(new LoadFromClasspath(), new TableTransformers()));
+    private final ParameterConverters converters = new ParameterConverters(new LoadFromClasspath(),
+            new TableTransformers());
+    private final Map<String, String> map = Collections.singletonMap("one", "11");
+    private final Parameters parameters = new ConvertedParameters(map, converters);
+
     private List<String> stringList;
     private List<Integer> integerList;
 
@@ -66,4 +72,73 @@ class ConvertedParametersBehaviour {
         assertThat(parameters.values(), is(map));
     }
 
+    @Test
+    void shouldConvertParametersToType() {
+        Parameters parameters = new ConvertedParameters(Collections.singletonMap("identifier", "id-1"),
+                converters);
+
+        assertThat(parameters.mapTo(Identifier.class).getIdentifier(), is("id-1"));
+    }
+
+    @Test
+    void shouldConvertParametersToTypeWithFieldMapping() {
+        Map<String, String> row = new HashMap<>();
+        row.put("years", "53");
+        row.put("firstName", "Boba");
+        row.put("l_name", "Fett");
+        row.put("identifier", "boba-fett-clone");
+
+        Parameters parameters = new ConvertedParameters(row, converters);
+
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("years", "age");
+        Person person = parameters.mapTo(Person.class, mapping);
+
+        assertThat(person.getIdentifier(), is("boba-fett-clone"));
+        assertThat(person.getAge(), is(53));
+        assertThat(person.getFirstName(), is("Boba"));
+        assertThat(person.getLastName(), is("Fett"));
+    }
+
+    @Test
+    void shouldThrowExceptionOnUnknownFieldWhileConversion() {
+        Parameters parameters = new ConvertedParameters(Collections.singletonMap("unknown_field", ""),
+                converters);
+
+        ParametersNotMappableToType thrown = assertThrows(ParametersNotMappableToType.class,
+                () -> parameters.mapTo(Identifier.class));
+        assertThat(thrown.getMessage(), is("Unable to map [unknown_field] field(s) for type class "
+                + "org.jbehave.core.steps.ConvertedParametersBehaviour$Identifier"));
+    }
+
+    public static class Identifier {
+
+        private String identifier;
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+    }
+
+    public static final class Person extends Identifier {
+
+        private int age;
+        private String firstName;
+        @Parameter(name = "l_name")
+        private String lastName;
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+    }
 }
