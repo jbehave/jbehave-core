1c9885b826b0cb95f441215d87b84404bbfdf585
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-01-18 11:49:25 +0300

    JBEHAVE-1533 Fix flaky examples

--- examples/core/src/main/java/org/jbehave/examples/core/steps/ParametrisationByDelimitedNameSteps.java
+++ examples/core/src/main/java/org/jbehave/examples/core/steps/ParametrisationByDelimitedNameSteps.java
@@ -8,33 +8,32 @@ import static org.hamcrest.Matchers.notNullValue;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.AfterScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
 public class ParametrisationByDelimitedNameSteps {
 
-    private String user;
-    private Set<String> products; // null means no cart...
+    private ThreadLocal<DataWrapper> data = ThreadLocal.withInitial(DataWrapper::new);
 
-    @BeforeScenario
-    public void init() {
-        user = null;
-        products = null;
+    @AfterScenario
+    public void tearDown() {
+        data.remove();
     }
 
     @Given("$user is logged in")
     public void loggedIn(String user) {
-        this.user = user;
+        getCurrentData().user = user;
     }
 
     @Given("$customer has a cart")
     public void aCustomerHasACart(String customer) {
-        assertThat(user, notNullValue());
-        assertThat(customer, equalTo(user));
-        if (products == null) {
-            products = new TreeSet<>();
+        DataWrapper currentData = getCurrentData();
+        assertThat(currentData.user, notNullValue());
+        assertThat(customer, equalTo(currentData.user));
+        if (currentData.products == null) {
+            currentData.products = new TreeSet<>();
         }
     }
 
@@ -45,13 +44,24 @@ public class ParametrisationByDelimitedNameSteps {
 
     @When("a $product is added to the cart")
     public void aProductIsAddedToCart(String product) {
+        Set<String> products = getCurrentData().products;
         assertThat(products, notNullValue());
         products.add(product);
     }
 
     @Then("cart contains $product")
     public void cartContains(String product) {
+        Set<String> products = getCurrentData().products;
         assertThat(products, notNullValue());
         assertThat(products.contains(product), is(true));
+    }
+
+    private DataWrapper getCurrentData() {
+        return this.data.get();
+    }
+
+    private static final class DataWrapper {
+        private String user;
+        private Set<String> products; // null means no cart...
     }
 }
