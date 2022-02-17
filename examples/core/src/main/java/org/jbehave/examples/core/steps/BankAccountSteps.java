882950684ad796ce4204f36a945994f74f9ecdf5
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-02-17 20:08:15 +0300

    JBEHAVE-1540 Fix flaky examples

--- examples/core/src/main/java/org/jbehave/examples/core/steps/BankAccountSteps.java
+++ examples/core/src/main/java/org/jbehave/examples/core/steps/BankAccountSteps.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jbehave.core.annotations.AfterStory;
 import org.jbehave.core.annotations.Alias;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
@@ -19,63 +20,63 @@ import org.jbehave.core.model.ExamplesTable;
 
 public class BankAccountSteps {
 
-    private BankAccount account;
-    private List<BankAccount> accounts = new ArrayList<>();
+    private final ThreadLocal<BankManager> bankManager = new ThreadLocal<>();
 
     @Given("I have a bank account")
     public void givenIHaveABankAccount() {
-        this.account = new BankAccount("Me");
+        bankManager.set(new BankManager(new BankAccount("Me")));
     }
 
     @Given("my balance is $balance")
     public void givenBalanceIs(int balance) {
-        account.setBalance(balance);
+        getMyAccount().setBalance(balance);
     }
 
     @When("I withdraw $value")
     public void whenIWithdraw(int value) {
-        account.withdraw(value);
+        getMyAccount().withdraw(value);
     }
 
     @Then("my bank account balance should be $balance")
     @Alias("my bank account balance should be <balance>")
-    public void thenBalanceShouldBe(@Named("balance")int balance) {
-        assertThat(account.getBalance(), equalTo(balance));
+    public void thenBalanceShouldBe(@Named("balance") int balance) {
+        assertThat(getMyAccount().getBalance(), equalTo(balance));
     }
 
     @When("I deposit $value")
     @Alias("I deposit <value>")
-    public void whenIDeposit(@Named("value")int value) {
-        account.deposit(value);
+    public void whenIDeposit(@Named("value") int value) {
+        getMyAccount().deposit(value);
     }
 
     @Given("these people have bank accounts with balances: $accountInfos")
-    public void givenPeopleHaveBankAccounts(@Named("accountInfos")ExamplesTable accountInfos) {
+    public void givenPeopleHaveBankAccounts(@Named("accountInfos") ExamplesTable accountInfos) {
         for (Map<String, String> info : accountInfos.getRows()) {
             final BankAccount account = new BankAccount(info.get("Name"));
             final int balance = Integer.parseInt(info.get("balance"));
             account.setBalance(balance);
-            accounts.add(account);
+            getBankManager().getAccounts().add(account);
         }
     }
 
     @When("I take all their money")
     public void whenIAddUp() {
-        for (BankAccount bankAccount : accounts) {
-            account.deposit(bankAccount.balance);
+        BankAccount myAccount = getMyAccount();
+        for (BankAccount bankAccount : getBankManager().getAccounts()) {
+            myAccount.deposit(bankAccount.balance);
         }
     }
 
     @Then("my balance is in credit")
     public void thenBalanceInCredit() {
-        assertThat(account.getBalance(), greaterThan(0));
+        assertThat(getMyAccount().getBalance(), greaterThan(0));
     }
 
     @Then("my balance is archived")
     public void thenBalanceIsArchived() {
         Date now = new Date();
-        account.archive(now);
-        assertThat(account.getArchived(now), equalTo(account.getBalance()));
+        getMyAccount().archive(now);
+        assertThat(getMyAccount().getArchived(now), equalTo(getMyAccount().getBalance()));
     }
 
     @Then("my balance is not archived")
@@ -84,14 +85,43 @@ public class BankAccountSteps {
 
     @Then("my balance is printed")
     public void thenBalanceIsPrinted() {
-        System.out.println(account.getBalance());
+        System.out.println(getMyAccount().getBalance());
     }
 
+    @AfterStory
+    public void cleanUp() {
+        bankManager.remove();
+    }
+
+    private BankAccount getMyAccount() {
+        return getBankManager().getMyAccount();
+    }
+
+    private BankManager getBankManager() {
+        return this.bankManager.get();
+    }
+
+    public static class BankManager {
+        private final BankAccount myAccount;
+        private final List<BankAccount> accounts = new ArrayList<>();
+
+        public BankManager(BankAccount myAccount) {
+            this.myAccount = myAccount;
+        }
+
+        public BankAccount getMyAccount() {
+            return myAccount;
+        }
+
+        public List<BankAccount> getAccounts() {
+            return accounts;
+        }
+    }
 
     public static class BankAccount {
         private int balance;
         private final String name;
-        private Map<Date,Integer> archive = new HashMap<>();
+        private final Map<Date,Integer> archive = new HashMap<>();
 
         public BankAccount(String name) {
             this.name = name;
@@ -117,12 +147,12 @@ public class BankAccountSteps {
             this.balance = balance;
         }
 
-        public void deposit(int v) {
-            this.balance += v;
+        public void deposit(int value) {
+            this.balance += value;
         }
 
-        public void withdraw(int v) {
-            this.balance -= v;
+        public void withdraw(int value) {
+            this.balance -= value;
         }
     }
 }
