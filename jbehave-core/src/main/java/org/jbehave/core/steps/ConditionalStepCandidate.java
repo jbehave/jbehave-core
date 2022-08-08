bbe466fb4eb1cc8417dd546e67a7b05cc8b33af4
Uladzislau Arlouski <uarlouski@gmail.com>
2022-08-08 19:53:01 +0300

    JBEHAVE-1553 Fix invalid object instance when performing parametrised step

--- jbehave-core/src/main/java/org/jbehave/core/steps/ConditionalStepCandidate.java
+++ jbehave-core/src/main/java/org/jbehave/core/steps/ConditionalStepCandidate.java
@@ -3,7 +3,6 @@ package org.jbehave.core.steps;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.jbehave.core.condition.StepConditionMatcher;
@@ -13,15 +12,15 @@ import org.jbehave.core.steps.StepCreator.ParametrisedStep;
 
 public class ConditionalStepCandidate extends StepCandidate {
 
-    private final List<Method> methods;
+    private final Map<Method, StepCreator> stepCreators;
     private final StepConditionMatcher stepConditionMatcher;
 
-    private ConditionalStepCandidate(String patternAsString, int priority, StepType stepType, List<Method> methods,
-            Class<?> stepsType, Keywords keywords, StepMatcher stepMatcher, String parameterPrefixString,
-            StepCreator stepCreator, StepConditionMatcher stepConditionMatcher) {
-        super(patternAsString, priority, stepType, null, stepsType, null, keywords, stepMatcher, parameterPrefixString,
+    private ConditionalStepCandidate(String patternAsString, int priority, StepType stepType,
+            Map<Method, StepCreator> stepCreators, Keywords keywords, StepMatcher stepMatcher,
+            String parameterPrefixString, StepCreator stepCreator, StepConditionMatcher stepConditionMatcher) {
+        super(patternAsString, priority, stepType, null, null, null, keywords, stepMatcher, parameterPrefixString,
                 stepCreator);
-        this.methods = methods;
+        this.stepCreators = stepCreators;
         this.stepConditionMatcher = stepConditionMatcher;
     }
 
@@ -32,21 +31,23 @@ public class ConditionalStepCandidate extends StepCandidate {
 
     @Override
     public Step createMatchedStep(String stepAsString, Map<String, String> namedParameters, List<Step> composedSteps) {
-        Map<Method, ParametrisedStep> parametrisedSteps = methods.stream()
-                .collect(Collectors.toMap(Function.identity(), m -> (ParametrisedStep) getStepCreator()
-                        .createParametrisedStep(m, stepAsString, stepAsString, namedParameters, composedSteps)));
+        Map<Method, ParametrisedStep> parametrisedSteps = stepCreators.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> (ParametrisedStep) e.getValue().createParametrisedStep(e.getKey(), stepAsString,
+                                stepAsString, namedParameters, composedSteps)));
 
         return getStepCreator().createConditionalStep(stepConditionMatcher, parametrisedSteps);
     }
 
     public static StepCandidate from(StepConditionMatcher stepConditionMatcher,
             List<StepCandidate> conditionalCandidates) {
-        List<Method> methods = conditionalCandidates.stream().map(StepCandidate::getMethod)
-                .collect(Collectors.toList());
+        Map<Method, StepCreator> stepCreators = conditionalCandidates.stream()
+                .collect(Collectors.toMap(StepCandidate::getMethod, StepCandidate::getStepCreator));
+
         StepCandidate baseCandidate = conditionalCandidates.get(0);
 
         StepCandidate candidate = new ConditionalStepCandidate(baseCandidate.getPatternAsString(),
-                baseCandidate.getPriority(), baseCandidate.getStepType(), methods, baseCandidate.getStepsType(),
+                baseCandidate.getPriority(), baseCandidate.getStepType(), stepCreators,
                 baseCandidate.getKeywords(), baseCandidate.getStepMatcher(),
                 baseCandidate.getParameterPrefix(), baseCandidate.getStepCreator(), stepConditionMatcher);
         candidate.useStepMonitor(baseCandidate.getStepMonitor());
