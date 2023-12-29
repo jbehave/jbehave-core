e087ad6a8cbd4a9dbb1c37952244e7aa1f22656a
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-12-29 21:58:35 +0200

    JBEHAVE-1599 Fix archetype sub-projects

--- archetypes/jbehave-needle-archetype/src/main/resources/archetype-resources/src/main/java/MyStories.java
+++ archetypes/jbehave-needle-archetype/src/main/resources/archetype-resources/src/main/java/MyStories.java
@@ -6,28 +6,20 @@ package ${package};
 import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Properties;
 import java.util.Set;
 
 import org.jbehave.core.Embeddable;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.i18n.LocalizedKeywords;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnit4StoryRunner;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.model.ExamplesTableFactory;
-import org.jbehave.core.model.TableTransformers;
-import org.jbehave.core.parsers.RegexPrefixCapturingPatternParser;
-import org.jbehave.core.parsers.RegexStoryParser;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
-import org.jbehave.core.steps.ParameterControls;
-import org.jbehave.core.steps.ParameterConverters;
+import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.ParameterConverters.DateConverter;
-import org.jbehave.core.steps.ParameterConverters.ExamplesTableConverter;
 import org.jbehave.core.steps.needle.NeedleStepsFactory;
 import org.junit.runner.RunWith;
 import org.needle4j.injection.InjectionProvider;
@@ -62,27 +54,15 @@ public class MyStories extends JUnitStories {
     @Override
     public Configuration configuration() {
         Class<? extends Embeddable> embeddableClass = this.getClass();
-        LoadFromClasspath resourceLoader = new LoadFromClasspath(embeddableClass);
-        TableTransformers tableTransformers = new TableTransformers();
-        ParameterControls parameterControls = new ParameterControls();
-        // Start from default ParameterConverters instance
-        ParameterConverters parameterConverters = new ParameterConverters(resourceLoader, tableTransformers);
-        // factory to allow parameter conversion and loading from external resources (used by StoryParser too)
-        ExamplesTableFactory examplesTableFactory = new ExamplesTableFactory(new LocalizedKeywords(), resourceLoader,
-                parameterConverters, parameterControls, tableTransformers);
+        Configuration configuration = new MostUsefulConfiguration()
+                .useStoryLoader(new LoadFromClasspath(embeddableClass))
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                        .withDefaultFormats()
+                        .withFormats(CONSOLE, TXT, HTML, XML));
         // add custom converters
-        parameterConverters.addConverters(new DateConverter(new SimpleDateFormat("yyyy-MM-dd")),
-                new ExamplesTableConverter(examplesTableFactory));
-        return new MostUsefulConfiguration()
-            .useStoryLoader(new LoadFromClasspath(embeddableClass))
-            .useStoryParser(new RegexStoryParser(examplesTableFactory)) 
-            .useStoryReporterBuilder(new StoryReporterBuilder()
-                .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
-                .withDefaultFormats()
-                .withFormats(CONSOLE, TXT, HTML, XML))
-            .useParameterConverters(parameterConverters)
-            .useParameterControls(parameterControls)
-            .useTableTransformers(tableTransformers);
+        configuration.parameterConverters().addConverters(new DateConverter(new SimpleDateFormat("yyyy-MM-dd")));
+        return configuration;
     }
     
     @Override
